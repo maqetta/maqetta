f4f7698d217237e0d07b00beb07ae730f82475fe
Wayne Vicknair <waynevicknair@gmail.com>
2011-05-12 16:52:47 -0500

    Bug 7775 - make build pull from GitHub

--- releng/davinci.releng/build.sh
+++ releng/davinci.releng/build.sh
@@ -4,44 +4,104 @@
 #  you must have some version of eclipse installed to build against
 #
 
-# parent of the eclipse directory being build against
-export base="/path/to/applications/directory"
+#
+# Path to parent directory of the eclipse application directory. 
+# That is, if eclipse is installed in '/usr/local/eclipse', this property 
+# would be set as 'base="/usr/local"'. No trailing slash.
+#
+export base="/Applications"
 
-# name of eclipse directory  (usually but not necessarily'eclipse'), has subdirs /plugin, /configuration, etc
-export baseLocation="${base}/eclipse"
+#
+# Path to eclipse directory inclusive. The application directory is 
+# usually, but not always, named 'eclipse'. It has sub-directories
+# /configuration, /features, /plugins, etc. No trailing slash.
+#
+export baseLocation="${base}/eclipse-3.6"
 
-# version number of the launcher jar file, look at  baseLocation/plugins/org.eclipse.equinox.launcher_*.jar, use just the number part
+#
+# Version number of the launcher jar file. See ${baseLocation}/plugins/org.eclipse.equinox.launcher_*.jar. 
+# The launcher version is the set of alphanumeric characters between 'launcher_' and the '.' character 
+# before the 'jar' file name suffix.
+#
 launcherVersion="1.1.1.R36x_v20101122_1400"
 
-# directory to do the build in
-export buildDirectory="/path/to/build/directory"
+#
+# Directory in which to do the build. No trailing slash.
+#
+export buildDirectory="/Users/wayne/work/build"
 
-# directory containing build.xml (this should not have to be changed in most cases)
-export relEngDir="${buildDirectory}/davinci.releng"
+#
+# Directory containing build.xml (this should not have to be changed in most cases).
+# No trailing slash.
+#
+export relEngDir="${buildDirectory}/repository/maqetta/releng/davinci.releng"
 
-# url of repository
-export svnRepository="https://xxxxxxxx.com/davinci/trunk"
-
-# svn user id (include --username)
-export svnUser="--username xxxx@xxx.ibm.com"
-
-# svn user password (include --password)
-export svnPassword="--password xxxxx"
-
-
-# if maqettaCode is set, copy from your local workspace instead of svn checkout
+#
+# If 'maqettaCode' is set, copy files from your local workspace instead of GitHub repository
+#
+# Note: This build feature is in incubation and *cannot* be used for production builds.
+#
 #export maqettaCode="/path/to/your/local/eclipse/workspace"
 
+#
+# GitHub URL for Maqetta repository. This should not change.
+#
+export gitRepository="git@github.com:maqetta/maqetta.git"
 
-# look at baseLocation/plugins/org.eclipse.equinox.launcher.xxx.yyy.xxx/  will be something like cocoa.macos.x86_64 
-export  myOS="xxx"
-export myWS="yyy"
-export myArch="zzz"
+#
+# SVN URL for javax.activation and javax.mail Eclipse projects
+#
+# Note: Users outside of IBM need to create these projects for
+#       themselves as they are *not* checked-in to GitHub
+#
+export svnRepository="https://lab5.austin.ibm.com/davinci"
 
+#
+# Windowing System, Operating System and processor Architecture settings
+#
+# Note: See ${baseLocation}/plugins/org.eclipse.equinox.launcher.xxx.yyy.xxx/  
+#       to determine your settings, they should be similar to 'cocoa.macosx.x86_64' 
+#
+export myWS="cocoa"
+export myOS="macosx"
+export myArch="x86_64"
+
+# 
+# Set up for and pull down the latest code from GitHub
+#
+if [ ! -d ${buildDirectory}/repository ]
+then
+    echo "Making repository directory"
+    mkdir -p ${buildDirectory}/repository
+fi
+
+#
+# If '.git' directory exists we need only pull
+#
+if [ -d ${buildDirectory}/repository/maqetta/.git ]
+then
+    echo "Doing 'git pull'..."
+    cd ${buildDirectory}/repository/maqetta
+    git pull
+else
+    echo "Cloning repository. This may take a few moments..."
+    cd ${buildDirectory}/repository
+    git clone ${gitRepository}
+    echo "Checking out javax.activation and javax.mail SVN projects..."
+    cd maqetta
+    svn co ${svnRepository}/trunk/javax.activation
+    svn co ${svnRepository}/trunk/javax.mail
+fi
+
+#
+# Change directory to the build directory.
+#
+# Note: Many scripts use relative directory references making
+#       running the build from this directory *imperative*.
+#
 cd ${buildDirectory}
-# check out the build files
-svn checkout --force ${svnUser} ${svnPassword} ${svnRepository}/releng/davinci.releng 
 
-
-
-java -jar ${baseLocation}/plugins/org.eclipse.equinox.launcher_${launcherVersion}.jar -application org.eclipse.ant.core.antRunner -buildfile ${relnEngDir}buildAll.xml   
+#
+# Run the Ant buildAll script from the davinci.releng project.
+#
+java -jar ${baseLocation}/plugins/org.eclipse.equinox.launcher_${launcherVersion}.jar -application org.eclipse.ant.core.antRunner -buildfile ${relEngDir}/buildAll.xml   

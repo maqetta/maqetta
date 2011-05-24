08e2b433e34e7dcd4ba9383cc247af0b9e796178
Adam L. Peller <github@peller.org>
2011-05-24 00:14:21 -0400

    Apply Phil's whitespace fix (in Dojo 1.7)

--- davinci.core/WebContent/dojox/highlight/languages/xml.js
+++ davinci.core/WebContent/dojox/highlight/languages/xml.js
@@ -10,7 +10,7 @@ dojo.require("dojox.highlight._base");
 	
 	var XML_ATTR = {
 		className: 'attribute',
-		begin: ' [a-zA-Z-]+=', end: '^',
+		begin: ' [a-zA-Z-]+\\s*=\\s*', end: '^',
 		contains: ['value']
 	};
 	

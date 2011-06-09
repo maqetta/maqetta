468c32a3df7d4c6eee316017b7d60b3fcc1b5de2
Cynthia Skach <cmskach@us.ibm.com>
2011-06-09 14:12:53 -0500

    Creating globalization branch

---
+++ davinci.core/WebContent/davinci/ve/nls/en-us/veLang.js
@@ -0,0 +1,25 @@
+({
+		//SwitchingStyleView.js
+		"common":"Common",
+		
+		"widgetSpecific":"Widget-specific",
+		
+		"events":"Events",
+		
+		"layout":"Layout",
+		
+		"showMinMax":"show min/max",
+		
+		"paddingMargins":"Padding/Margins",
+		"showtrbl":"show t/r/b/l",
+		
+		"background":"Background",
+		
+		"border":"Border",
+		"showDetails":"show details",
+		"fontsAndText":"Fonts and Text",
+		
+		//DeviceActions.js
+		"chooseDeviceSilhouette":"Choose a device silhouette: "
+
+})

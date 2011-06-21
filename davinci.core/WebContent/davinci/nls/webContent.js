dff33fcba34778071b8353e77a97c11f1fff5ffc
Cynthia Skach <cmskach@us.ibm.com>
2011-06-21 16:40:05 -0500

    Got a start on globalizing html files but still in testing phase

---
+++ davinci.core/WebContent/davinci/nls/webContent.js
@@ -0,0 +1,19 @@
+({
+		/*Currently this file is also used for davinci.review/WebContent/review.html*/
+		//pagedesigner.html
+		"welcome":"Welcome1",//also in review.html
+		"loadingMaqetta":"&nbsp;Loading Maqetta...",
+		"logOff":"Log Out1",//also in review.html
+		"unsupportedBrowser":"Unsupported Browser",
+		"unsupportedNote":"<p> Maqetta supports Firefox 4, Chrome and Safari 5. Please upgrade your browser to one of these versions. </p> <p> If you wish to continue with an unsupported browser, click \"Continue.\" </p>",
+		"buttonContinue":"Continue",
+		
+		//preview.html
+		"devicePreviewError":"Maqetta device previewer error. Must supply URL parameter 'file='",
+		
+		//davinci.review/WebContent/review.html
+		"help":"Help1",
+		"tutorials":"Tutorials1",
+		"aboutMaqetta":"About Maqetta1"
+
+})

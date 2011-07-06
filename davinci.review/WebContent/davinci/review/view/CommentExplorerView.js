7e89f17dcaeb15291263d94657b5249737e5e362
Eldon <eldonreturn@gmail.com>
2011-07-06 15:13:12 +0800

    Polished the UXD of the information tooltip which is popup when the mouse is hovering the Review folder.

--- davinci.review/WebContent/davinci/review/view/CommentExplorerView.js
+++ davinci.review/WebContent/davinci/review/view/CommentExplorerView.js
@@ -65,6 +65,18 @@ dojo.declare("davinci.review.view.CommentExplorerView", davinci.workbench.ViewPa
 		this.infoCardContent = dojo.cache("davinci" ,"review/widgets/templates/InfoCard.html");
 		if(davinci.review.Runtime.getRole()!="Designer")
 		dojo.style(this.toolbarDiv, "display", "none");
+		
+		// Customize dijit._masterTT so that it will not be closed when the cursor is hovring on it
+		if(!dijit._masterTT){ dijit._masterTT = new dijit._MasterTooltip(); }
+		this.connect(dijit._masterTT.domNode, "mouseover", function(){
+			if(this._delTimer){
+				clearTimeout(this._delTimer);
+				this._delTimer = null;
+			}
+		});
+		this.connect(dijit._masterTT.domNode, "mouseleave", function(){
+			this._lastAnchorNode && this._leave();
+		});
 	},
 	
 	_updateActionBar: function(item,context){
@@ -226,7 +238,7 @@ dojo.declare("davinci.review.view.CommentExplorerView", davinci.workbench.ViewPa
                 timePattern:'HH:mm:ss'
 			});
 			template.detail_creator = davinci.review.Runtime.getDesigner()
-						+ "&nbsp;(" + davinci.review.Runtime.getDesignerEmail() + ")";
+						+ "&nbsp;&lt" + davinci.review.Runtime.getDesignerEmail() + "&gt";
 			template.detail_files = "";
 			item.getChildren(function(children){ c = children; },true);
 			dojo.forEach(c, function(i){
@@ -242,7 +254,13 @@ dojo.declare("davinci.review.view.CommentExplorerView", davinci.workbench.ViewPa
 			item.closed ? template.detail_dueDate_class = "closed" : template.detail_dueDate_class = "notClosed";
 			
 			this._showTimer = setTimeout(dojo.hitch(this, function(){
+				if(this._delTimer){
+					clearTimeout(this._delTimer);
+					delete this._delTimer;
+				}
 				dijit.showTooltip(dojo.string.substitute(this.infoCardContent, template), node.rowNode);
+				this._lastAnchorNode = node;
+				delete this._showTimer;
 			}), 1000);
 		}
 		
@@ -253,7 +271,12 @@ dojo.declare("davinci.review.view.CommentExplorerView", davinci.workbench.ViewPa
 			clearTimeout(this._showTimer);
 			delete this._showTimer;
 		}
-		dijit.hideTooltip(node.rowNode);
+		if(this._lastAnchorNode){
+			this._delTimer = setTimeout(dojo.hitch(this, function(){
+				dijit.hideTooltip(this._lastAnchorNode.rowNode);
+				delete this._delTimer;
+			}), 1000);
+		}
 	},
 	
 	

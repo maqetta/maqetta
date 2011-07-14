e6b3c80b28efba9def3c7d7fd99cbad90cdf6586
JonFerraiolo <jferrai@us.ibm.com>
2011-07-14 10:42:47 -0700

    Capture onclick for various mobile widgets to prevent view transitions happening within Maqetta page editor. Fixes #705

---
+++ davinci.dojo_1_7/WebContent/metadata/dojox/mobile/ListItemHelper.js
@@ -0,0 +1,27 @@
+dojo.provide("davinci.libraries.dojo.dojox.mobile.ListItemHelper");
+dojo.require("davinci.ve.tools.CreateTool");
+
+
+dojo.declare("davinci.libraries.dojo.dojox.mobile.ListItemHelper", null, {
+
+	
+	create: function(widget, srcElement){
+		var dijitWidget = widget.dijitWidget;
+		if(dijitWidget && dijitWidget.anchorNode){
+			// Fix for #705.
+			// The ListItem widget's startup logic registers an onclick
+			// handler, and if the 'moveTo' property has a reference to a view,
+			// then this built-in onclick handler will launch an animated
+			// transition to make that view visible. This is good for runtime execution,
+			// but we don't want this onclick handler to execute in the page editor.
+			// So, register a "click" handler in the capture phase (happens before default bubble phase)
+			// that calls stopPropagation(), which prevents the ListItem's onclick logic from getting invoked.
+			// This allows event to bubble up to ancestor widgets, and therefore
+			// will be caught by Maqetta and will cause a selection action to occur.
+			dijitWidget.anchorNode.addEventListener("click",function(e){
+				e.stopPropagation();		
+			}, true);
+		}
+	}
+
+});
---
+++ davinci.dojo_1_7/WebContent/metadata/dojox/mobile/TabBarButtonHelper.js
@@ -0,0 +1,32 @@
+dojo.provide("davinci.libraries.dojo.dojox.mobile.TabBarButtonHelper");
+dojo.require("davinci.ve.tools.CreateTool");
+
+
+dojo.declare("davinci.libraries.dojo.dojox.mobile.TabBarButtonHelper", null, {
+
+	
+	create: function(widget, srcElement){
+		// Fix for #705.
+		// The TabBarButton widget's startup logic registers an onclick
+		// handler on one of its interior DOM nodes if there is a
+		// "moveTo" property that has a reference to a view.
+		// This built-in onclick handler will launch an animated
+		// transition to make that view visible. This is good for runtime execution,
+		// but we don't want this onclick handler to execute in the page editor.
+		// So, register a "click" handler in the capture phase (happens before default bubble phase)
+		// that calls stopPropagation(), which prevents the TabBarButton's onclick logic from getting invoked.
+		// This allows event to bubble up to ancestor widgets, and therefore
+		// will be caught by Maqetta and will cause a selection action to occur.
+		var dijitWidget = widget.dijitWidget;
+		if(dijitWidget){
+			var domNode = dijitWidget.domNode;
+			var mblTabBarButtonAnchorNode = dojo.query('.mblTabBarButtonAnchor',domNode)[0];
+			if(mblTabBarButtonAnchorNode){
+				mblTabBarButtonAnchorNode.addEventListener("click",function(e){
+					e.stopPropagation();		
+				}, true);
+			}
+		}
+	}
+
+});

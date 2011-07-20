cc81f0f6408c4d7e60f3345acd3106fcce88cc1c
Bill Reed <billreed@us.ibm.com>
2011-07-20 13:21:55 -0400

    #753 EdgeToEdgeDataList and RoundRecDataList stop click event on ListItem in designer
    Change-Id: Ifab01aa44284af946a0a2036216f8e9f01dbe649

--- davinci.dojo_1_7/WebContent/metadata/dojox/mobile/EdgeToEdgeDataListHelper.js
+++ davinci.dojo_1_7/WebContent/metadata/dojox/mobile/EdgeToEdgeDataListHelper.js
@@ -30,6 +30,7 @@ dojo.declare("davinci.libraries.dojo.dojox.mobile.EdgeToEdgeDataListHelper", nul
 			}
 		
 		}
+		this.stopOnClickListItems(widget);
 	
 	},
 
@@ -77,6 +78,27 @@ dojo.declare("davinci.libraries.dojo.dojox.mobile.EdgeToEdgeDataListHelper", nul
 		});
 	
 	
+	}, 
+	
+	stopOnClickListItems: function(widget){
+		var dijitWidget = widget.dijitWidget;
+		if(dijitWidget && dijitWidget.containerNode){
+			// Fix for #753.
+			// The ListItem widget's startup logic registers an onclick
+			// handler, and if the 'moveTo' property has a reference to a view,
+			// then this built-in onclick handler will launch an animated
+			// transition to make that view visible. This is good for runtime execution,
+			// but we don't want this onclick handler to execute in the page editor.
+			// So, register a "click" handler in the capture phase (happens before default bubble phase)
+			// that calls stopPropagation(), which prevents the ListItem's onclick logic from getting invoked.
+			// This allows event to bubble up to ancestor widgets, and therefore
+			// will be caught by Maqetta and will cause a selection action to occur.
+			dojo.query(".mblListItemAnchor", dijitWidget.containerNode).forEach(function(node, index, arr){
+				node.addEventListener("click",function(e){
+					e.stopPropagation();		
+				}, true);
+			});
+		}
 	}
 
 

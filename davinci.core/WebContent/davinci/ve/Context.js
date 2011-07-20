31adb8e2dc7f94c4450418c516fb43bb018b48c2
Javier Pedemonte <github@pedemonte.us>
2011-07-19 19:06:01 -0500

    Clean up mobile meta tag.
    Since 'user-scalable' is set to 'no', doesn't make sense
    to also specify 'maximum/minimum-scale'.
    
    #683

--- davinci.core/WebContent/davinci/ve/Context.js
+++ davinci.core/WebContent/davinci/ve/Context.js
@@ -388,7 +388,7 @@ dojo.declare("davinci.ve.Context", null, {
 	 */
 	_mobileMetaElement: {
 		name: 'viewport',
-		content: 'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no'
+		content: 'width=device-width, initial-scale=1.0, user-scalable=no'
 	},
 	
 	setMobileMeta: function(deviceName) {

9015d7d899354ff1a1fac2f5ea09a0bc91ea54d8
Bill Reed <billreed@us.ibm.com>
2011-07-22 17:32:42 -0400

    #751 MenuBarItems attached to body
    Change-Id: I0ddb82faf95ed0196dd2e40cf7b2a59619beebb5

---
+++ davinci.dojo_1_7/WebContent/metadata/dijit/PopupMenuBarItemHelper.js
@@ -0,0 +1,13 @@
+dojo.provide("davinci.libraries.dojo.dijit.PopupMenuBarItemHelper");
+
+dojo.declare("davinci.libraries.dojo.dijit.PopupMenuBarItemHelper", null, {
+
+	create: function(widget, srcElement){
+
+		if (widget.dijitWidget.popup)
+			widget.dijitWidget.popup.domNode._dvWidget.hidden=true; // this will hide the dijitMenu in designer
+
+	}
+	
+
+});

maqetta/maqetta

commit ade351f1600e5653cc62f5729d6d9145f9aab656

Fixes #784 (and an old bugzilla bug)  Fiddle with focus box only for top-level 100%x100% widgets so the focus box doesn't go off the page and cause scrollbars

davinci.core/WebContent/davinci/ve/Focus.js
@@ -93,14 +93,12 @@ dojo.declare("davinci.ve.Focus", dijit._Widget, {
 		}
 	},
 
-	resize: function(box){
+	resize: function(box, widget){
 		if(!box){
 			return;
 		}
 
 		var b = dojo.mixin(this._box, box);
-		b.w = box.w;
-		b.h = box.h;
 
 		// Adjust for size of border when near the top left corner of the screen
 		if(b.l < this.size){
@@ -124,13 +122,14 @@ dojo.declare("davinci.ve.Focus", dijit._Widget, {
 			b.h = 0;
 		}
 
-		// Right/bottom edge must stay on screen
+		// When a single widget is 100%x100%, right/bottom edge must stay on screen
 		var container = this._context.getContainerNode();
-		if(b.l + b.w > container.scrollWidth){
-			b.w = container.scrollWidth - this.size * 2;
-		}
-		if(b.t + b.h > container.scrollHeight){
+		if(widget.getParent().type == "html.body" && widget.domNode.style.height == "100%"){
 			b.h = container.scrollHeight - this.size * 2;
+		}
+
+		if(widget.getParent().type == "html.body" && widget.domNode.style.width == "100%"){
+			b.w = container.scrollWidth - this.size * 2;
 		}
 
 		var h = b.h + this.size * 2;

davinci.core/WebContent/davinci/ve/Context.js
@@ -15,7 +15,7 @@ dojo.declare("davinci.ve.Context", null, {
 	moduleLoader: null,
 	immediatePropertyUpdates: false,
 	
-	_subscriptions : [],
+	_subscriptions: [],
 	
 	// comma-separated list of modules to load in the iframe
 	_bootstrapModules: "dijit.dijit",
@@ -61,7 +61,7 @@ dojo.declare("davinci.ve.Context", null, {
 	/*
 	 * @returns the path to the file being edited
 	 */
-	getPath : function(){
+	getPath: function(){
 		
 		/*
 		 * FIXME:
@@ -232,7 +232,7 @@ dojo.declare("davinci.ve.Context", null, {
 		return davinci.resource.findResource(this.getDocumentLocation());
 	},
 
-	getLibraryBase : function(id, version){
+	getLibraryBase: function(id, version){
 		return davinci.library.getLibRoot(id,version, this.getResourcePath()) || "";
 	},
 
@@ -319,7 +319,7 @@ dojo.declare("davinci.ve.Context", null, {
 	
 	
 	
-	getMobileDevice : function(){
+	getMobileDevice: function(){
 		
 		var doc = this.getDocument();
 		var head = doc.getElementsByTagName("head")[0];
@@ -330,8 +330,9 @@ dojo.declare("davinci.ve.Context", null, {
 	},
 	
 	setMobileDevice: function (device){
-		if (!device)
+		if (!device) {
 			device = 'none';
+		}
 		var doc = this.getDocument();
 		var head = doc.getElementsByTagName("head")[0];
 		var htmlElement=this.getDocumentElement();
@@ -420,7 +421,7 @@ dojo.declare("davinci.ve.Context", null, {
 		}
 	},
 
-	themeChanged : function(){
+	themeChanged: function(){
 		var changed = true;
 		// check for false alarms to avoid reloading theme
 		var model = this.getModel();
@@ -464,10 +465,11 @@ dojo.declare("davinci.ve.Context", null, {
 		dojo.withDoc(this.getDocument(), "_setSource", this, [source, callback, scope]);
 	},
 
-	getDojoUrl : function(){
-		 var loc=davinci.Workbench.location();
-			if (loc.charAt(loc.length-1)=='/')
-				loc=loc.substring(0,loc.length-1);
+	getDojoUrl: function(){
+		var loc=davinci.Workbench.location();
+		if (loc.charAt(loc.length-1)=='/') {
+			loc=loc.substring(0,loc.length-1);
+		}
 			
 		if(document && document.getElementsByTagName){
 			var scripts = document.getElementsByTagName("script");
@@ -486,7 +488,7 @@ dojo.declare("davinci.ve.Context", null, {
 		
 	},
 	
-	getResourcePath : function(){
+	getResourcePath: function(){
 		var model = this.getModel();
 		var filename = model.fileName;
 		var path = new davinci.model.Path(filename);
@@ -1007,11 +1009,11 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 	},
 	
 	getStyle: function(){
-		return (this._header ? this._header.style : undefined);
+		return this._header ? this._header.style : undefined;
 	},
 
 	setStyle: function(style){
-		var values = (davinci.ve.widget.parseStyleValues(style) );
+		var values = (davinci.ve.widget.parseStyleValues(style));
 		if(this._header){
 			var oldValues = davinci.ve.widget.parseStyleValues(this._header.style);
 			if(oldValues){
@@ -1029,7 +1031,7 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 //		davinci.ve.widget.setStyleValues(this.container, values); //TODO
 	},
 
-	loadStyleSheet : function(url) {
+	loadStyleSheet: function(url) {
 		if (!url) {
 			return;
 		}
@@ -1056,7 +1058,7 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 		this._links.push(link);
 	},
 
-	addModeledStyleSheet : function(url, libBasePath) {
+	addModeledStyleSheet: function(url, libBasePath) {
 		this.loadStyleSheet(url);
 		if (!this.model.hasStyleSheet(url)) {
 			this.model.addStyleSheet(url);
@@ -1390,12 +1392,12 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 					this._connects.push(dojo.connect(focus, "onExtentChange", this, "onExtentChange"));
 					focus._connected = true;
 				}
-				focus.resize(state.box);
+				var w = this.getSelection();
+				focus.resize(state.box, w[0]);
 				focus.allow(state.op);
 				if(focus.domNode.parentNode != containerNode){
 					containerNode.appendChild(focus.domNode);
 				}
-				var w = this.getSelection();
 				focus.show(w[0],inline);
 			}else{ // hide
 				focus.hide();
@@ -1799,7 +1801,7 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 		
 		return selector;
 	},
-	getSelector : function( widget, target){
+	getSelector: function( widget, target){
 		// return rules based on metadata IE theme
 		
 		var theme = this.getThemeMeta();
@@ -1971,7 +1973,7 @@ console.info("Content Dojo version: "+ win.dojo.version.toString());
 					// XXX Nasty nasty nasty special case for dojo attribute thats
 					// required. Need to generalize in the metadata somehow.
 					this.addHeaderScript(url, {
-						'djConfig' : "parseOnLoad: true"
+						djConfig: "parseOnLoad: true"
 					});
 				}else{
 					this.addHeaderScript(url);

commit 1b0fb80a95a0b9c74814a85787a491349079cf65

Widget resize was largely broken by ade351f. Fixes #820

davinci.core/WebContent/davinci/ve/Focus.js
@@ -123,7 +123,7 @@ dojo.declare("davinci.ve.Focus", dijit._Widget, {
 		}
 
 		// When a single widget at the top-level is 100%x100%, right/bottom edge must stay on screen
-		if(widget.getParent().type == "html.body"){
+		if(widget && widget.getParent().type == "html.body"){
 			var container = this._context.getContainerNode();
 			if(widget.domNode.style.height == "100%"){
 				b.h = container.scrollHeight - this.size * 2;
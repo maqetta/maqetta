3954a2a7fe793b50ffc69b9733ab3b9cd774ce7e
Bill Reed <billreed@us.ibm.com>
2011-08-22 17:25:44 -0400

    #805 Copy/paste tree needs to duplicate the data store
    Change-Id: Icc9c8340502f4d395e15e611d906d266792cc486

--- davinci.dojo_1_7/WebContent/metadata/dijit/TreeCreateTool.js
+++ davinci.dojo_1_7/WebContent/metadata/dijit/TreeCreateTool.js
@@ -12,13 +12,20 @@ dojo.declare("davinci.libraries.dojo.dijit.TreeCreateTool", davinci.ve.tools.Cre
 	constructor: function(data){
 		this._resizable = "both";
 	},
-
+	
 	_create: function(args){
+		
+		var command = this._getCreateCommand(args);
+		this._context.getCommandStack().execute(command);
+		this._select(this._tree);
+	},
+
+	_getCreateCommand: function(args){
 		if(this._data.length !== 3){
 			return;
 		}
 		
-		var storeData = this._data[0]
+		var storeData = this._data[0];
 		var modelData = this._data[1];
 		var treeData = this._data[2];
 		
@@ -64,15 +71,10 @@ dojo.declare("davinci.libraries.dojo.dijit.TreeCreateTool", davinci.ve.tools.Cre
 		modelData.properties.id = modelId;
 		modelData.context = this._context;
 		
-		var treeId = davinci.ve.widget.getUniqueObjectId(treeData.type, this._context.getDocument());
 		if(!treeData.properties){
 			treeData.properties = { };
 		}
-		// <hack> Added to make new ve code happy, davinci.ve.widget.createWidget requires id in properties or context on data, but id didn't work when dragging second tree onto canvas so switched to context:
-		// node.id= (data.properties && data.properties.id) || data.context.getUniqueID(srcElement); 
-		//treeData.properties.id = treeId;
 		treeData.context = this._context;
-		// </hack>
 
 		var store = undefined;
 		var model = undefined;
@@ -107,9 +109,30 @@ dojo.declare("davinci.libraries.dojo.dijit.TreeCreateTool", davinci.ve.tools.Cre
 			command.add(new davinci.ve.commands.ResizeCommand(tree, args.size.w, args.size.h));
 		}
 		
-		this._context.getCommandStack().execute(command);
+		//this._context.getCommandStack().execute(command);
 
-		this._select(tree);
+		//this._select(tree);
+		this._tree = tree;
+		return command;
+	},
+	
+	addPasteCreateCommand: function(command, args){
+
+		this._context = this._data.context;
+		var model = this._data.properties.model;
+		var modelWidget = davinci.ve.widget.byId(model.id);
+		var modelData = modelWidget.getData();
+		var storeWidget = davinci.ve.widget.byId(model.store._edit_object_id);
+   		var storeData = storeWidget.getData();
+   		var data = [];
+   		data[0] = storeData;
+   		data[1] = modelData;
+   		data[2] = this._data;
+   		this._data = data;
+   		command.add( this._getCreateCommand(args));
+   		return this._tree;
+
 	}
+	
 
 });

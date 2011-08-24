1e05b8dcba0329ccbdafd1d920c827f7a7903960
Bill Reed <billreed@us.ibm.com>
2011-08-24 12:38:37 -0400

    #116 Key mappings for in inline edit box
    Change-Id: If4ab53e9b47ca6ad90def4b35b8bacd197873eac

--- davinci.core/WebContent/davinci/ve/input/SmartInput.js
+++ davinci.core/WebContent/davinci/ve/input/SmartInput.js
@@ -473,14 +473,20 @@ dojo.declare("davinci.ve.input.SmartInput", null, {
 	
 	handleEvent: function(event){
 
-		if (event.keyCode == 13) {
-			var multiLine = this.multiLine;
-			if (!multiLine || multiLine == "false" || this._lastKeyCode == 13){ // back to back CR
-				this.hide();
-			}
-		} else {
-			this.updateFormats();
-		}
+	    switch (event.keyCode)
+	    {
+    	    case 13: // enter
+    	        var multiLine = this.multiLine;
+                if (!multiLine || multiLine == "false" || this._lastKeyCode == 13 || event.ctrlKey){ // back to back CR or CTRL+ENTER
+                    this.onOk();
+                }
+                break;
+    	    case 27: // ESC
+    	        this.onCancel();
+    	        break;
+    	    default:
+    	        this.updateFormats();
+	    }
 		this._lastKeyCode = event.keyCode;
 		this.updateSimStyle();
 	},

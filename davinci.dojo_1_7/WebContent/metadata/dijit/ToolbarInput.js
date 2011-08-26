0cd64cec4892ce59139d4d29d294723ffd622a5b
Bill Reed <billreed@us.ibm.com>
2011-08-26 14:31:50 -0400

    #95 HTML markup for TitlePane and Toolbar
    Change-Id: I84bed9f771e477fb2962ad468c6ac77d3a7021e3

--- davinci.dojo_1_7/WebContent/metadata/dijit/ToolbarInput.js
+++ davinci.dojo_1_7/WebContent/metadata/dijit/ToolbarInput.js
@@ -4,5 +4,6 @@ dojo.require("davinci.libraries.dojo.dijit.layout.ContainerInput");
 dojo.declare("davinci.libraries.dojo.dijit.ToolbarInput", davinci.libraries.dojo.dijit.layout.ContainerInput, {
 
 	propertyName: "label",
+	supportsHTML: true
 	
 });

5373db5a6cb2978dcd69e336915fe6b93204ab0d
Bill Reed <billreed@us.ibm.com>
2011-09-07 17:39:52 -0400

    checking for wrong class name for themeEditor #906

--- davinci.core/WebContent/davinci/ve/States.js
+++ davinci.core/WebContent/davinci/ve/States.js
@@ -116,7 +116,7 @@ dojo.declare("davinci.ve.States", davinci.maqetta.States, {
 		
 			this.subscribe("/davinci/states/state/changed", dojo.hitch(this, function(e) { 
 				var editor = this.getEditor();
-				if (!editor || editor.declaredClass == "davinci.themeEditor.ThemeEditor"){ return; } // ignore updates in theme editor
+				if (!editor || editor.declaredClass == "davinci.ve.themeEditor.ThemeEditor"){ return; } // ignore updates in theme editor
 
 				dojo.publish("/davinci/states/state/changed/start");
 				// If rootWidget, then loop through children, else loop starting with this widget.

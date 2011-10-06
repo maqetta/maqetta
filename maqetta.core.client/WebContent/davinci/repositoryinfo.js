5469791f520f4d9e332aa9200ea5218d9d50c390
Adam L. Peller <github@peller.org>
2011-10-06 17:52:01 -0400

    trivial AMD conversion

--- maqetta.core.client/WebContent/davinci/repositoryinfo.js
+++ maqetta.core.client/WebContent/davinci/repositoryinfo.js
@@ -1,6 +1,1 @@
-/*  build properties for about.js*/
-
-dojo.provide('davinci.repositoryinfo');
-
-davinci.repositoryinfo.revision='@revision@';
-davinci.repositoryinfo.buildtime='@buildtime@';
+define({revision: '@revision@', buildtime: '@buildtime'});

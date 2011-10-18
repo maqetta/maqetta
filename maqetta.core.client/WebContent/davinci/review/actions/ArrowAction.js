074edb3bc89ee49ceb5846fb28f27f2364f37533
Wayne Vicknair <waynevicknair@gmail.com>
2011-10-18 11:04:50 -0500

    add files

---
+++ maqetta.core.client/WebContent/davinci/review/actions/ArrowAction.js
@@ -0,0 +1,20 @@
+dojo.provide("davinci.review.actions.ArrowAction");
+
+dojo.require("davinci.review.actions._DrawingCommon");
+
+dojo.declare("davinci.review.actions.ArrowAction", davinci.review.actions._DrawingCommon, {
+	run: function(context){
+		this.inherited(arguments);
+		var commentPalette = dijit.byId("davinci.review.comment");
+		if(!commentPalette._commentForm.isShowing) return;
+		var surface = this.doc.annotationSurface, createTool = surface.createTool;
+		createTool.deactivate();
+		createTool.setShape("Arrow", {
+			colorAlias: surface.currentReviewer,
+			a2c: dojo.hitch(davinci.review.Runtime, davinci.review.Runtime.getColor),
+			commentId: surface.commentId,
+			state: ""
+		});
+		createTool.activate();
+	}
+});

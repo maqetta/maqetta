79e6f508f9e2d508136cbd32e2f77faeafbedcb5
Adam L. Peller <github@peller.org>
2011-11-15 10:11:44 -0500

    finish moving popup overlay logic from Context to helpers

--- davinci.dojo_1_7/WebContent/metadata/dijit/TooltipDialogHelper.js
+++ davinci.dojo_1_7/WebContent/metadata/dijit/TooltipDialogHelper.js
@@ -1,23 +1,44 @@
 define([
-    "dijit/popup"
-], function(popup){
+    "dojo/_base/connect"
+], function(connect){
 return function() {
-	this.popup = function(widget){
+	this.destroyWidget = function(widget) {
+		if (this.handle) {
+			connect.unsubscribe(this.handle);
+			delete this.handle;
+		}
+		widget.dijitWidget.destroyRecursive();
+	};
+
+	/*
+	 * Called by Outline palette whenever user toggles visibility by clicking on eyeball.
+	 * @param {davinci.ve._Widget} widget  Widget whose visibility is being toggled
+	 * @param {boolean} on  Whether given widget is currently visible
+	 * @return {boolean}  whether standard toggle processing should proceed
+	 */
+	this.onToggleVisibility = function(widget, on) {
+		return false;
+	};
+
+	this.onSelect = function(widget) {
 		var owner = widget.dijitWidget.owner;
 		if (owner) {
 			owner.openDropDown();
 		}
-	};
 
-	this.tearDown = function(widget, selected){
-		for(var w = selected; w && w != widget; w = w.getParent && w.getParent());
-		if(w != widget){
-			var owner = widget.dijitWidget.owner;
-			if (owner) {
-				owner.closeDropDown();
+		var id = widget.dijitWidget.id,
+		context = widget.getContext();
+		this.handle = connect.subscribe("/davinci/ui/widgetSelected", null, function(selected) {
+			for(var w = selected[0]; w && w != widget; w = w.getParent && w.getParent());
+			if(!w || w.id != id) {
+				var owner = context.getDijit().registry.byId(id).owner;
+				if (owner) {
+					owner.closeDropDown();
+				}
+				connect.disconnect(this.handle);
+				delete this.handle;
 			}
-		}
-		return w != widget;
+		}.bind(this));
 	};
 };
 });

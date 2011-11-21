496ab35434333d4475841ccdb339f6f499e6fe00
Javier Pedemonte <javier@pedemonte.us>
2011-11-20 18:33:44 -0600

    shapes/Hello must inherit from _WidgetBase.

--- maqetta.shapes/WebContent/shapes/Hello.js
+++ maqetta.shapes/WebContent/shapes/Hello.js
@@ -1,14 +1,14 @@
 define([
 	"dojo/_base/declare",
-	"shapes/Hello"
-], function(declare, Hello){
+	"dijit/_WidgetBase"
+], function(declare, _WidgetBase){
 	
 	//	module:
 	//		shapes/Hello
 	//	summary:
 	//		Drawing tools Hello
 		
-	return declare("shapes.Hello", [Hello], {
+	return declare("shapes.Hello", [_WidgetBase], {
 		// summary:
 		// 		Hello world shapes widget
 			

aa175c4fb55eb30ef43f68c2a0e993eff69219cb
Adam L. Peller <github@peller.org>
2011-12-01 20:56:16 -0500

    Fix #1315

--- davinci.dojo_1_7/WebContent/metadata/dijit/MenuHelper.js
+++ davinci.dojo_1_7/WebContent/metadata/dijit/MenuHelper.js
@@ -1,5 +1,6 @@
 define([
-], function() {
+	"dojo/_base/connect"
+], function(connect) {
 return function() {
 	this.getData = function(/*Widget*/ widget, /*Object*/ options) {
 		// summary:
@@ -80,12 +81,35 @@ return function() {
 		// use listener logic like dialog to trigger this code when child widgets are selected
 		widget._visibility = widget.domNode.style.visibility;
 		widget.domNode.style.visibility = "visible";
+
+		var id = widget.dijitWidget.id,
+		context = widget.getContext();
+		//FIXME: not safe to use 'this' since the helper instance does not correspond to the widget
+		this.handle = connect.subscribe("/davinci/ui/widgetSelected", null, function(selected) {
+			var w = selected[0];
+			while (w && w.id != id) {
+				if (w._ownerId) {
+					w = context.getDijit().registry.byId(w._ownerId);
+				} else {
+					w = w.getParent && w.getParent();
+				}
+			}
+
+			if (!w || w.id != id) {
+				widget.domNode.style.visibility = widget._visibility;
+				delete widget._visibility;
+				connect.disconnect(this.handle);
+				delete this.handle;
+			}
+		}.bind(this));
 	};
 
-	this.onDeselect = function(widget) {
-		widget.domNode.style.visibility = widget._visibility;
-		delete widget._visibility;
-		return true;
+	this.destroy = function(widget) {
+		if (this.handle) {
+			connect.unsubscribe(this.handle);
+			delete this.handle;
+		}
+		widget.dijitWidget.destroyRecursive();
 	};
 
 	this.getWidgetTextExtra = function(widget) {

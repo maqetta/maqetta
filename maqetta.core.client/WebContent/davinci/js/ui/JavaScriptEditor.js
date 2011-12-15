8261729648096a14c14ba5dd14a975c4d512f6e9
Wayne Vicknair <waynevicknair@gmail.com>
2011-12-15 09:48:17 -0600

    formatting for consistency in preparation for fixing

--- maqetta.core.client/WebContent/davinci/js/ui/JavaScriptEditor.js
+++ maqetta.core.client/WebContent/davinci/js/ui/JavaScriptEditor.js
@@ -7,31 +7,21 @@ dojo.require("davinci.js.ui.JavaScriptOutline");
 
 
 dojo.declare("davinci.js.ui.JavaScriptEditor", davinci.ui.ModelEditor, {
-	
+    
+    constructor : function (element) {
+        this.jsFile=davinci.model.Factory.newJS();
+        this.model=this.jsFile;
+    },
 
-	
-	  constructor : function (element) {
-	this.jsFile=new davinci.js.JSFile();
-	this.model=this.jsFile;
-//		this.inherited(arguments);
-	},
-	
-	getOutline : function ()
-	{
-		if (!this.outline)
-			this.outline=new davinci.js.ui.JavaScriptOutline(this.model);
-		return this.outline;
-	},
-	
-	
+    getOutline : function () {
+        if (!this.outline)
+            this.outline=new davinci.js.ui.JavaScriptOutline(this.model);
+        return this.outline;
+    },
 
-	getDefaultContent : function ()
-	{
-		return "function functionName ()\n{\n}\n";
-	}
-
-
-
+    getDefaultContent : function () {
+        return "function functionName ()\n{\n}\n";
+    }
 });
 
 

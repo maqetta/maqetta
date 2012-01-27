0e4614418473d351ea4e95ef4064bf7dd8e2629f
Adam L. Peller <github@peller.org>
2012-01-27 18:38:03 -0500

    remove States reference

--- maqetta.core.client/WebContent/davinci/ve/commands/StyleCommand.js
+++ maqetta.core.client/WebContent/davinci/ve/commands/StyleCommand.js
@@ -2,7 +2,7 @@ define([
     	"dojo/_base/declare",
     	"davinci/ve/widget"
 //    	"davinci/ve/States" // circular dep
-], function(declare, Widget, States){
+], function(declare, Widget){
 
 
 return declare("davinci.ve.commands.StyleCommand", null, {
@@ -69,7 +69,7 @@ return declare("davinci.ve.commands.StyleCommand", null, {
 			this._refresh(widget);
 			
 			// Recompute styling properties in case we aren't in Normal state
-			States.resetState(widget);
+			veStates.resetState(widget);
 		}
 		
 		//FIXME: Various widget changed events (/davinci/ui/widget*Changed) need to be cleaned up.
@@ -114,7 +114,7 @@ return declare("davinci.ve.commands.StyleCommand", null, {
 		this._refresh(widget);
 		
 		// Recompute styling properties in case we aren't in Normal state
-		States.resetState(widget);
+		davinci.ve.states.resetState(widget);
 		
 		//FIXME: Various widget changed events (/davinci/ui/widget*Changed) need to be cleaned up.
 		// I defined yet another one here (widgetPropertiesChanged) just before Preview3

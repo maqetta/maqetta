maqetta/maqetta

commit bb4a311b279aab01792f1e03a679e30796690671

Remove "davinci.libraries" namespace.
No longer used.
Don't use declare() for helpers that don't inherit.
Also fix some lint errors.

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalRuleLabelsHelper.js
@@ -2,7 +2,9 @@ define([
 "dojo/_base/declare"
 ], function(declare){
 
-return declare("davinci.libraries.dojo.dijit.form.HorizontalRuleLabelsHelper", null, {
+var HorizontalRuleLabelsHelper = function() {};
+
+HorizontalRuleLabelsHelper.prototype = {
 
 	preProcessData: function(data) {
 		// process labels if user makes changes and adjust Count
@@ -14,17 +16,19 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalRuleLabelsHelper", n
 				data.properties.count = strArray.length;
 				// numericMargin > 0 only if labels aren't specified
 				data.properties.numericMargin = 0; 
-			} else if (data.properties.count && data.properties.labels.length != parseInt(data.properties.count)
-					|| data.properties.numericMargin) {
+			} else if (data.properties.count &&
+					data.properties.labels.length != parseInt(data.properties.count, 10) ||
+					data.properties.numericMargin) {
 				// remove labels if user changes Count so that it can calculate the percentages for the Labels
-				if (parseInt(data.properties.count) > 1)
+				if (parseInt(data.properties.count, 10) > 1) {
 					delete data.properties.labels;
-				else {// Dojo doesn't handle a count of just 1
+				} else { // Dojo doesn't handle a count of just 1
 					data.properties.labels = new Array("50%");
 				}
 			}
 		}
 		return data;
 	}
-});
+};
+
 });

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalSliderHelper.js
@@ -2,7 +2,9 @@ define([
 "dojo/_base/declare"
 ], function(declare){
 
-return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderHelper", null, {
+var HorizontalSliderHelper = function() {};
+
+HorizontalSliderHelper.prototype = {
 
 	/**
 	 * HorizontalSlider, due to its support for placing rules and labels in the
@@ -48,5 +50,6 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderHelper", null,
 	_getDecoration: function(dijitWidget) {
 		return dijitWidget.topDecoration;
 	}
-});
+};
+
 });

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalSliderInput.js
@@ -14,7 +14,7 @@ define(
 function(declare, ContainerInput, ContentPane, BorderContainer, LayoutContainer,
 		HorizontalSlider, HorizontalRule, HorizontalRuleLabels, langObj, dijitLangObj) {
 
-return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderInput", ContainerInput, {
+return declare(ContainerInput, {
 
 	//Data structure to hold entries for (potentially) new children of the slider
 	_sliderChildrenEntries: null,
@@ -84,6 +84,7 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderInput", Contai
 	hide: function(cancel) {
 		if (this._inline) {
 			//Clean up connections
+			var connection;
 			while (connection = this._connection.pop()){
 				dojo.disconnect(connection);
 			}
@@ -231,9 +232,8 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderInput", Contai
 	
 	_getPreviewContent: function(){
 		//Construct template for slider based on dialog settings
-		var s =
-				'<div dojoType="' + this._getWidgetTypeForSlider()
-						+ '" style="' + this._getWidgetStyleForSlider() + '">'; 
+		var s = '<div dojoType="' + this._getWidgetTypeForSlider() + '" style="' +
+				this._getWidgetStyleForSlider() + '">'; 
 		
 		//Add child rules and labels
 		for(var i=0; i<this._sliderChildrenEntries.length; i++){
@@ -321,7 +321,7 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderInput", Contai
 		var value = null;
 		if (selectVal === "rules") {
 			value = this._getWidgetTypeForRule();
-		} else if (value = "labels") {
+		} else if (value === "labels") {
 			value = this._getWidgetTypeForRuleLabels();
 		} else {
 			console.log('WARNING. HorizontalSliderInput.js _getTypeFromSelectValue(). Invalid combo box value: val = ' + selectVal);
@@ -583,7 +583,7 @@ return declare("davinci.libraries.dojo.dijit.form.HorizontalSliderInput", Contai
 		} else {
 			props = {
 				"container": "bottomDecoration",
-				"style": "height:20px;",
+				"style": "height:20px;"
 			};
 		}
 		return props;

davinci.dojo_1_7/WebContent/metadata/dijit/form/VerticalSliderHelper.js
@@ -1,11 +1,11 @@
 define(
 [
 	"dojo/_base/declare", 
-	"./HorizontalSliderHelper", 
+	"./HorizontalSliderHelper"
 ],
 function(declare, HorizontalSliderHelper) {
 
-return declare("davinci.libraries.dojo.dijit.form.VerticalSliderHelper", HorizontalSliderHelper, {	
+return declare(HorizontalSliderHelper, {	
 	
 	_getDecoration: function(dijitWidget) {
 		return dijitWidget.leftDecoration;

davinci.dojo_1_7/WebContent/metadata/dijit/form/VerticalSliderInput.js
@@ -9,7 +9,7 @@ define(
 ],
 function(declare, HorizontalSliderInput, VerticalSlider, VerticalRule, VerticalRuleLabels, langObj) {
 
-return declare("davinci.libraries.dojo.dijit.form.VerticalSliderInput", HorizontalSliderInput, {	
+return declare(HorizontalSliderInput, {	
 	
 	_getDialogTitle: function() {
 		return langObj.verticalSliderDialog;
@@ -48,7 +48,7 @@ return declare("davinci.libraries.dojo.dijit.form.VerticalSliderInput", Horizont
 		} else {
 			props = {
 				"container": "rightDecoration",
-				"style": "width:3em;",
+				"style": "width:3em;"
 			};
 		}
 		return props;

commit 443af50efe00a841ec717a9f5e7f249b43342eff

Fixes #1754 (Sliders broken after bb4a311b279aab01792f1e03a679e30796690671). HorizontalSliderHelper's definition is missing: return HorizontalSliderHelper;

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalRuleLabelsHelper.js
@@ -31,4 +31,6 @@ HorizontalRuleLabelsHelper.prototype = {
 	}
 };
 
+return HorizontalRuleLabelsHelper;
+
 });

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalSliderHelper.js
@@ -52,4 +52,6 @@ HorizontalSliderHelper.prototype = {
 	}
 };
 
+return HorizontalSliderHelper;
+
 });

davinci.dojo_1_7/WebContent/metadata/dijit/form/HorizontalSliderInput.js
@@ -321,7 +321,7 @@ return declare(ContainerInput, {
 		var value = null;
 		if (selectVal === "rules") {
 			value = this._getWidgetTypeForRule();
-		} else if (value === "labels") {
+		} else if (selectVal === "labels") {
 			value = this._getWidgetTypeForRuleLabels();
 		} else {
 			console.log('WARNING. HorizontalSliderInput.js _getTypeFromSelectValue(). Invalid combo box value: val = ' + selectVal);
b1fa98083e4e54d38d1b999c839953c75406549c
Tony Erwin <aerwin@us.ibm.com>
2012-02-16 17:15:14 -0500

    Add smart input for SELECT HTML tag (#1602)

--- davinci.html/WebContent/metadata/html/nls/html.js
+++ davinci.html/WebContent/metadata/html/nls/html.js
@@ -3,6 +3,8 @@ define({ root: {
 		//html/HtmlSrcAttributeInput.js
 		"typeFileUrl":"File URL:",
 		"typeAltText":"Alt text:",
-		"selectSource":"Select a source"
-
+		"selectSource":"Select a source",
+		
+		//html/SelectInput.js
+		"selectInputHelp":"Enter multiple lines of text. Each line will correspond to an option item in the list. Indicate the default option to be selected with the \"+\" character at the start of the line. One level of nesting is supported. Indicate a nsted level by putting a \">\" character at the start of a line. "
 }});

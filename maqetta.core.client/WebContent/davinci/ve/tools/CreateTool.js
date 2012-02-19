maqetta/maqetta

commit 6e1bdff810a75429e1cac1e7b3425b746997a38b

Fix #1768 Make sure createTool path calls require on all helpers in the widget prior to placing on the page, similar to what Context does for preProcess.  getHelper() call expects it to be loaded.

maqetta.core.client/WebContent/davinci/ve/tools/CreateTool.js
@@ -4,6 +4,7 @@ define(["dojo/_base/declare",
 		"davinci/workbench/Preferences",
 		"../metadata",
 		"../widget",
+		"dojo/DeferredList",
 		"davinci/ui/ErrorDialog",
 		"davinci/commands/CompoundCommand",
 		"../commands/AddCommand",
@@ -19,6 +20,7 @@ define(["dojo/_base/declare",
 		Preferences,
 		Metadata,
 		Widget,
+		DeferredList,
 		ErrorDialog
 ) {
 
@@ -40,10 +42,10 @@ return declare("davinci.ve.tools.CreateTool", _Tool, {
 
 	activate: function(context){
 		this._context = context;
-		if(this._context && this._context.rootNode){
-			this._oldCursor = this._context.rootNode.style.cursor;
+		if(context && context.rootNode){
+			this._oldCursor = context.rootNode.style.cursor;
 		}
-		this._context.rootNode.style.cursor = "crosshair";
+		context.rootNode.style.cursor = "crosshair";
 	},
 
 	deactivate: function(){
@@ -350,7 +352,7 @@ return declare("davinci.ve.tools.CreateTool", _Tool, {
     	        var library = Metadata.getLibraryForType(type),
     	            libId = library.name,
     	            args = [type, context];
-    	        if (! context._widgets.hasOwnProperty(libId)) {
+    	        if (!context._widgets.hasOwnProperty(libId)) {
     	            context._widgets[libId] = 0;
     	        }
     	        if (++context._widgets[libId] == 1) {
@@ -534,11 +536,54 @@ return declare("davinci.ve.tools.CreateTool", _Tool, {
 //			}
 //		}
 		this._data.context=this._context;
-		this._create({parent: parent, index: index, position: position, size: args.size});
+
+		var requireHelpers = function(data){
+			var promises = [];
+			if(!data || !data.type){
+				if (data instanceof Array) {
+					data.forEach(function(d) {
+						promises.concat(requireHelpers(d));
+					});
+				}
+				return promises;
+			}
+
+			promises.push(Widget.requireWidgetHelper(data.type));
+
+			if(data.children && !dojo.isString(data.children)){
+				if(!dojo.every(data.children, function(c){
+					return promises.concat(requireHelpers(c));
+				})){
+					return promises;
+				}
+			}
+			return promises;
+		};
+
+		new DeferredList(requireHelpers(this._data)).then(function() {
+			this._create({parent: parent, index: index, position: position, size: args.size});			
+		}.bind(this));
 	},
 
 	_create: function(args){
-		if(!this._loadType(this._data)){
+		var loadType = function(data){
+			if(!data || !data.type){
+				return false;
+			}
+			if(!data._context.loadRequires(data.type,true)){
+				return false;
+			}
+			if(data.children && !dojo.isString(data.children)){
+				if(!dojo.every(data.children, function(c){
+					return loadType(c);
+				})){
+					return false;
+				}
+			}
+			return true;
+		}
+
+		if(!loadType(this._data)){
 			return;
 		}
 
@@ -589,24 +634,6 @@ return declare("davinci.ve.tools.CreateTool", _Tool, {
 				this._context.select(w); // no inline on create
 			}
 		}.bind(this));
-	},
-
-	_loadType: function(data){
-		
-		if(!data || !data.type){
-			return false;
-		}
-		if( !this._context.loadRequires(data.type,true)){
-			return false;
-		}
-		if(data.children && !dojo.isString(data.children)){
-			if(!dojo.every(data.children, function(c){
-				return this._loadType(c);
-			}, this)){
-				return false;
-			}
-		}
-		return true;
 	}
 });
 

maqetta.core.client/WebContent/davinci/ve/tools/PasteTool.js
@@ -17,10 +17,8 @@ define(["dojo/_base/declare",
 return declare("davinci.ve.tools.PasteTool", CreateTool, {
 
 	_create: function(args){
-		var command = new CompoundCommand(),
-			index = args.index,
-			baseline,
-			selection = [];
+		var index = args.index,
+			baseline;
 		dojo.forEach(this._data, function(d){
 			var loadRequiresForTree = dojo.hitch(this, function(d){
 				if(d.type){ // structure has plain 'string' nodes which don't have type or children
@@ -55,12 +53,15 @@ return declare("davinci.ve.tools.PasteTool", CreateTool, {
 				}
 			}
 
-			var w;
 			dojo.withDoc(this._context.getDocument(), function(){
 				d.context = this._context;
 				metadata.getHelper(d.type, "tool").then(function(ToolCtor) {
-					var myTool;
-				    if (ToolCtor) {
+					var w,
+						myTool,
+						selection = [],
+						command = new CompoundCommand();
+
+					if (ToolCtor) {
 				    	myTool = new ToolCtor(d);
 			        }
 			        if (myTool && myTool.addPasteCreateCommand) {
@@ -87,22 +88,18 @@ return declare("davinci.ve.tools.PasteTool", CreateTool, {
 					if (position) {
 						command.add(new MoveCommand(w, position.x, position.y));
 					}
-				});
-			}, this);
-			if (!w) {
-				return;
-			}
 
-			selection.push(w);
+					selection.push(w);
+
+					if(!command.isEmpty()){
+						this._context.getCommandStack().execute(command);
+						dojo.forEach(selection, function(w, i){
+							this._context.select(w, i > 0);
+						}, this);
+					}
+				}.bind(this));
+			}, this);
 		}, this);
-
-		if(!command.isEmpty()){
-			this._context.getCommandStack().execute(command);
-			dojo.forEach(selection, function(w, i){
-				this._context.select(w, i > 0);
-			}, this);
-		}
 	}
-
 });
 });

commit 7ee289ec431b89851beb9cc7564fb23b27e39759

fix context reference, regression from 6e1bdff810a75429e1cac1e7b3425b746997a38b

maqetta.core.client/WebContent/davinci/ve/tools/CreateTool.js
@@ -570,7 +570,7 @@ return declare("davinci.ve.tools.CreateTool", _Tool, {
 			if(!data || !data.type){
 				return false;
 			}
-			if(!data._context.loadRequires(data.type,true)){
+			if(!data.context.loadRequires(data.type,true)){
 				return false;
 			}
 			if(data.children && !dojo.isString(data.children)){
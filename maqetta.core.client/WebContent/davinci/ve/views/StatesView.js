432de5ae91b891e12afe2d9458367a6156f32437
JonFerraiolo <jferrai@us.ibm.com>
2012-02-25 06:11:43 -0800

    Get theme editor working new Scenes palette.

--- maqetta.core.client/WebContent/davinci/ve/views/StatesView.js
+++ maqetta.core.client/WebContent/davinci/ve/views/StatesView.js
@@ -344,8 +344,26 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			}
 		}
 		this._store.save();
-		
-		//FIXME: Need to update theme editor, too, with Grid->Tree changes
+
+		var storedScenes = this._getScenes();
+
+		// Build an object structure that contains the latest list of states/scenes/views
+		// We will then build a similar object structure by extracting the list from the ItemFileWriteStore
+		// and then compare the two to see if there are any changes
+		var fileName = (this._editor && this._editor.fileName) ? this._editor.fileName : 'file';
+		var CurrentFileObj = {name:fileName, type:'file', category:'file', children:[]};
+		var AppStatesObj = {name:'Widget States', type:'SceneManagerRoot', category:'AppStates', children:[]};
+		var latestData = [CurrentFileObj];
+		for(var state in latestStates){
+			AppStatesObj.children.push({ name:state, type:'AppState' });
+		}
+		CurrentFileObj.children.push(AppStatesObj);
+
+		// If data in Tree widget is same as latest data, then just return
+		if(!this._compareStructures(latestData, storedScenes)){
+			// Destroy the old Tree widget and create a new Tree widget
+			this._destroyCreateTree(latestData);
+		}
 	},
 	
 	_updateList: function() {
@@ -370,8 +388,6 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			}
 		}
 		this._store.save();
-
-		var that = this;
 		
 		// Build an object structure that contains the latest list of states/scenes/views
 		// We will then build a similar object structure by extracting the list from the ItemFileWriteStore
@@ -416,136 +432,12 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			CurrentFileObj.children.push(AppStatesObj);
 		}
 		
-		// The following inner functions are used to see if we need
-		// to recreate the tree widget because the list of states or 
-		// the plugin scene managers has different data.
-		function compareProperty(o1, o2, prop){
-			if((o1[prop] && !o2[prop]) || (!o1[prop] && o2[prop])){
-				return false;	// return false if objects don't match
-			}
-			// Dojo's datastores puts values as first elements of array, hence [0]
-			if(o1[prop] && o1[prop][0] !== o2[prop][0]){
-				return false;	// return false if objects don't match
-			}
-			return true;
-		}
-		function compareObjectRecursive(o1, o2){
-			var props = ['sceneId','name','type','category'];
-			for(var pidx = 0; pidx < props.length; pidx++){
-				var p = props[pidx];
-				if(!compareProperty(o1, o2, p)){
-					return false;
-				}
-			}
-			if((o1.children && !o2.children) || (!o1.children && o2.children)){
-				return false;	// return false if objects don't match
-			}
-			if(o1.children){
-				if(!compareArray(o1.children, o2.children)){
-					return false;	// return false if objects don't match
-				}
-			}
-			return true;
-		}
-		function compareArray(a1, a2){
-			if(a1.length != a2.length){
-				return false; 	// return false if objects don't match
-			}
-			for(var i=0; i<a1.length; i++){
-				if(!compareObjectRecursive(a1[i], a2[i])){
-					return false;	// return false if objects don't match
-				}
-			}
-			return true;
-		}
-		
 		// If data in Tree widget is same as latest data, then just return
-		if(compareArray(latestData, storedScenes)){
-			return;
+		if(!this._compareStructures(latestData, storedScenes)){
+			// Destroy the old Tree widget and create a new Tree widget
+			this._destroyCreateTree(latestData);
 		}
 		
-		// The following logic recreates the data stores and the Tree
-		if(this._tree){
-			this._tree.destroyRecursive();
-			this._forest.destroy();
-		}
-	    var skeletonData = { identifier: 'id', label: 'name', items: []};
-		this._sceneStore = new ItemFileWriteStore({ data: skeletonData, clearOnClose:true });
-		this._forest = new ForestStoreModel({ store:this._sceneStore, query:{type:'file'},
-			  rootId:'StoryRoot', rootLabel:'All', childrenAttrs:['children']});
-		this._tree = new Tree({model:this._forest, showRoot:false, autoExpand:true, class:'StatesViewTree', style:'height:150px', 
-			_createTreeNode:function(args){
-				var item = args.item;
-				if(item.type && item.category && item.category[0] === 'AppStates'){
-					// Custom TreeNode class (based on dijit.TreeNode) that allows rich text labels
-					return new that.RichHTMLTreeNode(args);
-				}else{
-					// Custom TreeNode class (based on dijit.TreeNode) that uses default plain text labels
-					return new that.PlainTextTreeNode(args);
-				}
-			},
-			getIconClass: function(/*dojo.data.Item*/ item, /*Boolean*/ opened){
-				return "dijitLeaf";
-			}
-		});
-		this.centerPane.domNode.appendChild(this._tree.domNode);	
-		dojo.connect(this._tree, "onClick", this, function(item){
-			var currentEditor = this._editor;
-			var context = currentEditor ? currentEditor.getContext() : null;
-			var bodyWidget = context ? context.rootWidget : null;
-			if (item && item.type && item.type[0] == 'AppState') {
-				if (this.isThemeEditor()){
-					if (!this._silent) {
-						this.publish("/davinci/states/state/changed", [{widget:'$all', newState:item.name[0], oldState:this._themeState, context: this._editor.context}]);
-					}
-					this._themeState = item.name[0];
-					this._silent = false;
-				} else {
-					if(context && bodyWidget){
-						var state = item.name[0];
-						States.setState(bodyWidget, state);
-						context.deselectInvisible();
-					}
-				}
-			}else{
-				if(bodyWidget){
-					States.setState(bodyWidget, null);
-				}
-				if(item.sceneId){
-					// Loop through plugin scene managers, eg Dojo Mobile Views
-					for(var smIndex in sceneManagers){
-						var sm = sceneManagers[smIndex];
-						if(sm.selectScene){
-							if(sm.selectScene({ sceneId:item.sceneId[0]})){
-								break;
-							}
-						}
-					}
-				}
-			}
-		});
-		function newItemRecursive(obj, parentItem){
-			var o = dojo.mixin({}, obj);
-			var id = that.nextId+'';
-			that.nextId++
-			o.id = id;		// ensure unique ID
-			delete o.children;	// remove children property before calling newItem
-			var thisItem;
-			if(parentItem){
-				thisItem = that._sceneStore.newItem(o, {parent:parentItem, attribute:'children'});
-			}else{
-				thisItem = that._sceneStore.newItem(o);
-			}
-			if(obj.children){
-				obj.children.forEach(function(child){
-					newItemRecursive(child, thisItem);
-				});
-			}
-		}
-		latestData.forEach(function(obj){
-			newItemRecursive(obj);
-		});
-		this._sceneStore.save();
 	},
 	
 	_updateSelection: function() {
@@ -656,6 +548,146 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			})
 		});
 		this._store.save();
+	},
+	
+	/**
+	 * Compare two hierarchical lists to see if they have the same set of nested
+	 * objects and those objects have the same set of properties.
+	 * The two data structures match this construct:
+	 * [{prop1:..., prop2:..., children:[{[prop1:..., prop2:..., children:[...]}]{]}]
+	 */
+	_compareStructures: function(a1, a2){
+		// The following inner functions are used to see if we need
+		// to recreate the tree widget because the list of states or 
+		// the plugin scene managers has different data.
+		function compareProperty(o1, o2, prop){
+			if((o1[prop] && !o2[prop]) || (!o1[prop] && o2[prop])){
+				return false;	// return false if objects don't match
+			}
+			// Dojo's datastores puts values as first elements of array, hence [0]
+			if(o1[prop] && o1[prop][0] !== o2[prop][0]){
+				return false;	// return false if objects don't match
+			}
+			return true;
+		}
+		function compareObjectRecursive(o1, o2){
+			var props = ['sceneId','name','type','category'];
+			for(var pidx = 0; pidx < props.length; pidx++){
+				var p = props[pidx];
+				if(!compareProperty(o1, o2, p)){
+					return false;
+				}
+			}
+			if((o1.children && !o2.children) || (!o1.children && o2.children)){
+				return false;	// return false if objects don't match
+			}
+			if(o1.children){
+				if(!compareArray(o1.children, o2.children)){
+					return false;	// return false if objects don't match
+				}
+			}
+			return true;
+		}
+		function compareArray(a1, a2){
+			if(a1.length != a2.length){
+				return false; 	// return false if objects don't match
+			}
+			for(var i=0; i<a1.length; i++){
+				if(!compareObjectRecursive(a1[i], a2[i])){
+					return false;	// return false if objects don't match
+				}
+			}
+			return true;
+		}
+		return compareArray(a1, a2);
+	},
+	
+	_destroyCreateTree: function(latestData){
+		var that = this;
+		
+		// The following logic recreates the data stores and the Tree
+		if(this._tree){
+			this._tree.destroyRecursive();
+			this._forest.destroy();
+		}
+	    var skeletonData = { identifier: 'id', label: 'name', items: []};
+		this._sceneStore = new ItemFileWriteStore({ data: skeletonData, clearOnClose:true });
+		this._forest = new ForestStoreModel({ store:this._sceneStore, query:{type:'file'},
+			  rootId:'StoryRoot', rootLabel:'All', childrenAttrs:['children']});
+		this._tree = new Tree({model:this._forest, showRoot:false, autoExpand:true, class:'StatesViewTree', style:'height:150px', 
+			_createTreeNode:function(args){
+				var item = args.item;
+				if(item.type && item.category && item.category[0] === 'AppStates'){
+					// Custom TreeNode class (based on dijit.TreeNode) that allows rich text labels
+					return new that.RichHTMLTreeNode(args);
+				}else{
+					// Custom TreeNode class (based on dijit.TreeNode) that uses default plain text labels
+					return new that.PlainTextTreeNode(args);
+				}
+			},
+			getIconClass: function(/*dojo.data.Item*/ item, /*Boolean*/ opened){
+				return "dijitLeaf";
+			}
+		});
+		this.centerPane.domNode.appendChild(this._tree.domNode);	
+		dojo.connect(this._tree, "onClick", this, function(item){
+			var currentEditor = this._editor;
+			var context = currentEditor ? currentEditor.getContext() : null;
+			var bodyWidget = context ? context.rootWidget : null;
+			if (item && item.type && item.type[0] == 'AppState') {
+				if (this.isThemeEditor()){
+					if (!this._silent) {
+						this.publish("/davinci/states/state/changed", [{widget:'$all', newState:item.name[0], oldState:this._themeState, context: this._editor.context}]);
+					}
+					this._themeState = item.name[0];
+					this._silent = false;
+				} else {
+					if(context && bodyWidget){
+						var state = item.name[0];
+						States.setState(bodyWidget, state);
+						context.deselectInvisible();
+					}
+				}
+			}else{
+				if(bodyWidget){
+					States.setState(bodyWidget, null);
+				}
+				if(item.sceneId){
+					// Loop through plugin scene managers, eg Dojo Mobile Views
+					for(var smIndex in sceneManagers){
+						var sm = sceneManagers[smIndex];
+						if(sm.selectScene){
+							if(sm.selectScene({ sceneId:item.sceneId[0]})){
+								break;
+							}
+						}
+					}
+				}
+			}
+		});
+		function newItemRecursive(obj, parentItem){
+			var o = dojo.mixin({}, obj);
+			var id = that.nextId+'';
+			that.nextId++
+			o.id = id;		// ensure unique ID
+			delete o.children;	// remove children property before calling newItem
+			var thisItem;
+			if(parentItem){
+				thisItem = that._sceneStore.newItem(o, {parent:parentItem, attribute:'children'});
+			}else{
+				thisItem = that._sceneStore.newItem(o);
+			}
+			if(obj.children){
+				obj.children.forEach(function(child){
+					newItemRecursive(child, thisItem);
+				});
+			}
+		}
+		latestData.forEach(function(obj){
+			newItemRecursive(obj);
+		});
+		this._sceneStore.save();
 	}
+
 });
 });

417ea772062afd58e5e52cfbaeb62318973d9a0b
JonFerraiolo <jferrai@us.ibm.com>
2012-02-25 11:27:21 -0800

    Remove all of the old datagrid code from StatesView.js

--- maqetta.core.client/WebContent/davinci/ve/views/StatesView.js
+++ maqetta.core.client/WebContent/davinci/ve/views/StatesView.js
@@ -55,9 +55,6 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 		this.subscribe("/davinci/scene/removed", this._removeScene.bind(this));
 		this.subscribe("/davinci/scene/renamed", this._renameScene.bind(this));
 		this.subscribe("/davinci/scene/selectionChanged", this._sceneSelectionChanged.bind(this));
-/*
-		this._createStateList();
-*/
 	},
 	
 	_contextLoaded: function() {
@@ -191,81 +188,6 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 	},
 */
 
-/*
-	_createStateList: function() {
-
-		// Setup our data store:
-		var statesData = {
-			identifier: "id",
-			"items": []
-		};
-		this._store = new ItemFileWriteStore({ data: statesData });
-		
-		dojo.connect(this._store, "onSet", function(item, attribute, oldValue, newValue){
-			if (oldValue !== newValue) {
-				States.rename(oldValue, newValue);
-			}
-		});
-
-		// Set the layout structure:
-		var layout = [{
-			field: 'name',
-			name: 'name',
-			width: '100%',
-			editable: true
-		}];
-		
-		// Create a new grid:
-		this._grid = DataGrid({
-				id: "dvStatesDataGrid",
-				store: this._store,
-				structure: layout,
-				selectionMode: "single",
-				updateDelay: 0,
-				autoHeight:true,
-				style:'min-height:60px',
-				canEdit: function(cell, rowIndex) {
-					return rowIndex !== 0;
-				}
-			}
-		);
-		dojo.connect(this._grid, "onSelected", this, function(index){
-			var item = this._grid.getItem(index);
-			if (item) {
-				if (this.isThemeEditor()){
-					if (!this._silent) {
-						this.publish("/davinci/states/state/changed", [{widget:'$all', newState:item.name[0], oldState:this._themeState, context: this._editor.context}]);
-					}
-					this._themeState = item.name[0];
-					this._silent = false;
-				} else {
-					var currentEditor = this._editor;
-					var context = currentEditor.getContext();
-					var bodyWidget = context.rootWidget;
-					if(context && bodyWidget){
-						var state = item.name[0];
-						States.setState(bodyWidget, state);
-						context.deselectInvisible();
-					}
-				}
-			}
-		});
-		
-		// Append the new grid to the div container:
-		//this.centerPane.domNode.appendChild(this._grid.domNode);	
-
-		// Call startup, in order to render the grid:
-		this._grid.startup();
-	},
-
-	_getStateList: function() {
-		if (!this._grid) {
-			this._createStateList();
-		}
-		return this._grid;
-	},
-*/
-
 	_updateView: function() {
 		var context = this._editor.getContext();
 		if(!context._statesLoaded){
@@ -292,29 +214,6 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			}
 		}
 		latestStates = names;
-/*
-		storedStates = this._getStates();
-		
-		// Remove all stored states not in latestStates
-		for (var name in storedStates) {
-			var state = storedStates[name];
-			if (!latestStates[name]) {
-				this._store.deleteItem(state);
-			}
-		}
-		
-		// Add states from latestStates not yet in stored states
-		for (var name in latestStates) {
-			var storedState = storedStates[name];
-			if (!storedState) {
-				var id = this.nextId+'';
-				this.nextId++
-				var newState = { name: name, id: id };
-				this._store.newItem(newState);
-			}
-		}
-		this._store.save();
-*/
 		var storedScenes = this._getScenes();
 
 		// Build an object structure that contains the latest list of states/scenes/views
@@ -342,32 +241,7 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 	
 	_updateList: function() {
 		var latestStates = States.getStates(this._getWidget(), true), 
-			storedScenes = this._getScenes()
-/*
-			,
-			storedStates = this._getStates()
-*/
-			;
-
-/*
-		// Remove all stored states not in latestStates
-		for (var name in storedStates) {
-			var state = storedStates[name];
-			if (!latestStates[name]) {
-				this._store.deleteItem(state);
-			}
-		}
-		
-		// Add states from latestStates not yet in stored states
-		for (var name in latestStates) {
-			var storedState = storedStates[name];
-			if (!storedState) {
-				var newState = { name: name, id: name };
-				this._store.newItem(newState);
-			}
-		}
-		this._store.save();
-*/
+			storedScenes = this._getScenes();
 		
 		// Build an object structure that contains the latest list of states/scenes/views
 		// We will then build a similar object structure by extracting the list from the ItemFileWriteStore
@@ -451,21 +325,6 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 		if(sceneId){
 			this._updateSelectedScene('AppState', sceneId);
 		}
-
-/*
-		this._store.fetch({query: {name:"*"}, onComplete: dojo.hitch(this, function(items, request){
-				for (var i = 0; i < items.length; i++){
-					var item = items[i];
-					var state = item.name[0];
-					if (state == currentState) {
-						selectionIndex = i;
-					}
-				}
-			})
-		});
-		this._getStateList().selection.clear();
-		this._getStateList().selection.addToSelection(selectionIndex);
-*/
 	},
 
 	_updateThemeSelection: function(currentState, silent) {
@@ -491,44 +350,7 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			}
 			this._updateSelectedScene('AppState', sceneId);
 		}
-
-/*Comment out temporarily - need to get this working again
-		var selectionIndex = 0;
-		if (!currentState) return;
-		var items = this._grid._by_idx;
-		for (var i = 0; i < items.length; i++){
-			var item = items[i].item;
-			var state = item.name[0];
-			if (state == currentState) {
-				selectionIndex = i;
-				break;
-			}
-		}
-
-		this._grid.selection.clear();
-		if (silent == undefined){
-			this._silent = true;
-		} else {
-			this._silent = silent;
-		}
-		this._grid.selection.addToSelection(selectionIndex);
-*/
 	},
-
-/*
-	_getStates: function() {
-		var names = {};
-		this._store.fetch({query: {name:"*"}, onComplete: dojo.hitch(this, function(items, request){
-				for (var i = 0; i < items.length; i++){
-					var item = items[i];
-					var name = item.name[0];
-					names[name] = item;
-				}
-			})
-		});
-		return names;		
-	},
-*/
 
 	_getScenes: function() {
 		var scenes = [];
@@ -556,36 +378,7 @@ return declare("davinci.ve.views.StatesView", [ViewPart], {
 			})});
 		}
 		return scenes;		
-		/*
-		var ids = {};
-		if(this._sceneStore){
-			this._sceneStore.fetch({query:{}, queryOptions:{deep:true}, onComplete:dojo.hitch(this, function(items, request){
-				for (var i = 0; i < items.length; i++){
-					var item = items[i];
-					var id = item.id[0];
-					ids[id] = item;
-				}
-			})});
-		}
-		return ids;		
-		*/
 	},
-
-/*
-	_clearList: function() {
-		this._getStateList().selection.clear();
-
-		var request = this._store.fetch({query: {name:"*"}, onComplete: dojo.hitch(this, function(items, request){
-				for (var i = 0; i < items.length; i++){
-					var item = items[i];
-					var id = item.name[0];
-					this._store.deleteItem(item);
-				}
-			})
-		});
-		this._store.save();
-	},
-*/
 	
 	/**
 	 * Compare two hierarchical lists to see if they have the same set of nested

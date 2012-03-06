ae927b6cc321e62c1a2797f67f581620dbd126a9
bradley childs <childsb@us.ibm.com>
2012-03-06 17:24:11 -0600

    remove debugger; stmt.

--- maqetta.core.client/WebContent/davinci/ve/VisualEditor.js
+++ maqetta.core.client/WebContent/davinci/ve/VisualEditor.js
@@ -198,7 +198,6 @@ var VisualEditor = declare("davinci.ve.VisualEditor", null, {
 			
 			for(var i=0;i<value.values.length;i++){
 				for(var name in value.values[i]){
-					debugger;
 					if(URLRewrite.containsUrl(value.values[i][name]) && !URLRewrite.isAbsolute(value.values[i][name])){
 						
 						var oldUrl = new Path(URLRewrite.getUrl(value.values[i][name]));

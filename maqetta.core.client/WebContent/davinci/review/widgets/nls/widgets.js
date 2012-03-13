31e2064a3b3a991882b6f4328ba7d6ac75c21f1e
JonFerraiolo <jferrai@us.ibm.com>
2012-03-13 11:44:21 -0700

    Cosmetic tweaks to instructions message above review editors.

--- maqetta.core.client/WebContent/davinci/review/widgets/nls/widgets.js
+++ maqetta.core.client/WebContent/davinci/review/widgets/nls/widgets.js
@@ -80,6 +80,6 @@ define({ root:
 		"mailFailureMsg":"It seems that the mail service is down or not configured. Please copy the invitation below and send it manually.",
 		
 		//ReviewToolBarText.js
-		'ReviewToolBarText':'Note: The page below is a "live" web application (e.g., may respond to mouse clicks or touches).The page might consist of different "scenes"; if so, then you can change the currently active scene in the Scenes palette.'
+		'ReviewToolBarText':'Note: The page below is a "live" web application (e.g., may respond to mouse clicks or touches). You can add comments and on-screen annotations using the Comments palette. You can change the currently active scene in the Scenes palette.'
 }
 });

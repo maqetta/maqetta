0ebf506cc725864bca13e4368ff5b2dc7280ba68
Adam L. Peller <github@peller.org>
2012-03-16 14:51:19 -0400

    #1918 one more time.  check the array length, not the var.

--- maqetta.core.client/WebContent/davinci/ui/widgets/ToggleTree.js
+++ maqetta.core.client/WebContent/davinci/ui/widgets/ToggleTree.js
@@ -54,7 +54,7 @@ define(["dojo/_base/declare",
 				return path;
 			});
 			try{
-				if (paths && paths[0].length) {
+				if (paths.length && paths[0].length) {
 					this.set('paths', paths);					
 				}
 			}catch(e){

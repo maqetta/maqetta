12d20d8b39e97fe5479415c4dceac5385f8dc8f5
bradley childs <childsb@us.ibm.com>
2012-03-19 15:16:45 -0500

    Fix for #1494 - open R&C editor after login when clicking on a link.

--- maqetta.core.client/WebContent/davinci/Workbench.js
+++ maqetta.core.client/WebContent/davinci/Workbench.js
@@ -79,7 +79,7 @@ var getSelectedResource = function() {
 };
 
 var initializeWorkbenchState = function(){
-	
+
 	if (!Workbench._state || !Workbench._state.hasOwnProperty("editors")) {
 		Workbench._state = Runtime.serverJSONRequest({
 			url: "cmd/getWorkbenchState",
@@ -124,6 +124,7 @@ var initializeWorkbenchState = function(){
 		}
 	
 		for (var i=0;i<state.editors.length;i++) {
+		
 			var isReviewRes = isReview(state.editors[i]);
 			if(isReviewRes){
 				var reviewProject = getReviewProject(state.editors[i]);
@@ -142,7 +143,7 @@ var initializeWorkbenchState = function(){
 			if(isReviewRes){
 				var version = getReviewVersion(state.editors[i]);
 				var resPath = getReviewResource(state.editors[i]).toString();
-				resource = davinci.review.model.resource.root.findFile(version, "./" + resPath);
+				resource = davinci.review.model.resource.root.findFile(version, resPath);
 			}else{
 				resource = sysResource.findResource(state.editors[i]);
 			}
@@ -1362,6 +1363,7 @@ console.warn('_switchEditor before publish /davinci/ui/editorSelected');
 console.warn('_switchEditor after publish /davinci/ui/editorSelected');
 		} catch (ex) {console.error(ex);}
 		Workbench._updateTitle(newEditor);
+	
 		Workbench._state.activeEditor=newEditor ? newEditor.fileName : null;
 	
 		if(newEditor) {
@@ -1447,6 +1449,7 @@ console.warn('_switchEditor exit');
 
 	_editorTabClosed: function(page) {
 		if (page && page.editor && page.editor.fileName) {
+		
 			util.arrayRemove(Workbench._state.editors, page.editor.fileName);
 			Workbench._updateWorkbenchState();
 		}

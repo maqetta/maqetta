4724bf4567e2756eb24ac0c13b683924d85911c3
Wayne Vicknair <waynevicknair@gmail.com>
2012-03-21 14:55:51 -0500

    if the plugin sending the login command is joomla try to auto-create user if they don't exist.

--- maqetta.core.server/src/maqetta/core/server/command/Login.java
+++ maqetta.core.server/src/maqetta/core/server/command/Login.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.davinci.server.user.IUser;
+import org.davinci.server.user.UserException;
 import org.maqetta.server.Command;
 import org.maqetta.server.IDavinciServerConstants;
 import org.maqetta.server.ServerManager;
@@ -20,24 +21,41 @@ public class Login extends Command {
         String password = req.getParameter("password");
         user = ServerManager.getServerManger().getUserManager().login(name, password);
         if (user != null) {
-            String redirect = (String) req.getSession().getAttribute(IDavinciServerConstants.REDIRECT_TO);
-            req.getSession().removeAttribute(IDavinciServerConstants.REDIRECT_TO); // burn after reading
-            this.responseString = (redirect != null) ? redirect : "OK";
-            HttpSession session = req.getSession(true);
-            session.setAttribute(IDavinciServerConstants.SESSION_USER, user);
-            session.setMaxInactiveInterval(IDavinciServerConstants.SESSION_TIMEOUT);
-            Cookie k = new Cookie(IDavinciServerConstants.SESSION_USER, user != null ? user.getUserName() : null);
-    		k.setPath("/maqetta");
-    		resp.addCookie(k);
-    	    
-            
+        	storeUserInSession(req, resp, user);
         } else {
             user = ServerManager.getServerManger().getUserManager().getUser(name);
             if (user == null) {
-                resp.sendError(HttpServletResponse.SC_FORBIDDEN, "User not known");
+                String pluginName = req.getParameter("plugin");
+            	if (pluginName == "joomla") { // auto-create user to work around joomla user synchronization issues.
+                    String email = req.getParameter("email");
+                    try {
+						user = ServerManager.getServerManger().getUserManager().addUser(name, password, email);
+					} catch ( UserException e ) {
+						resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.getReason());
+						return;
+					}
+                    user = ServerManager.getServerManger().getUserManager().login(name, password);
+					if (user != null) {
+			        	storeUserInSession(req, resp, user);
+					} else {
+	                	resp.sendError(HttpServletResponse.SC_FORBIDDEN, "User not known");
+					}
+            	} else {
+                	resp.sendError(HttpServletResponse.SC_FORBIDDEN, "User not known");
+            	}
             } else {
                 resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Incorrect username/password");
             }
         }
+    }
+    
+    private void storeUserInSession(HttpServletRequest req, HttpServletResponse resp, IUser user) {
+        this.responseString = "OK";
+        HttpSession session = req.getSession(true);
+        session.setAttribute(IDavinciServerConstants.SESSION_USER, user);
+        session.setMaxInactiveInterval(IDavinciServerConstants.SESSION_TIMEOUT);
+        Cookie k = new Cookie(IDavinciServerConstants.SESSION_USER, user != null ? user.getUserName() : null);
+		k.setPath("/maqetta");
+		resp.addCookie(k);
     }
 }

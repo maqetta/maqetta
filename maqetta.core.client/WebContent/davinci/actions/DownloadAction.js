afb190fe064bbafdd0e2d5624378f70306db5598
Tony Erwin <aerwin@us.ibm.com>
2012-05-03 13:01:59 -0500

    Sanitize file path in download scenario (Bugzilla 7974)
    Added validation on the client side and sanitization on the server side
    (in case someone calls the URL from outside of Maqetta).
    
    Also, consolidated client side to use the same dialog for both the
    right-click "Download" action and the toolbar "Download" Selected
    action.

--- maqetta.core.client/WebContent/davinci/actions/DownloadAction.js
+++ maqetta.core.client/WebContent/davinci/actions/DownloadAction.js
@@ -2,57 +2,23 @@ define([
         "dojo/_base/declare",
     	"./Action",
     	"../Workbench",
+    	"../ui/DownloadSelected",
     	"system/resource",
     	"davinci/ui/Resource",
     	"dijit/Dialog",
-    	"dojo/i18n!./nls/actions"
-], function(declare, Action, Workbench, resource, uiResource, Dialog, langObj){
+    	"dojo/i18n!./nls/actions",
+    	"dijit/form/ValidationTextBox"
+], function(declare, Action, Workbench, DownloadSelected, resource, uiResource, Dialog, langObj){
 
 return declare("davinci.actions.DownloadAction", Action, {
 
 	run: function() {
-		var files = uiResource.getSelectedResources();
-		this._files = files;
-		var filesDiv = "";
-		for ( var i = 0; i < files.length; i++) {
-			filesDiv += "<div>" + files[i].getPath() + "</div>";
-		}
-		var proposedFileName = "download";
-		var formHtml =
-			"<div >" +
-		    '<div>'+ langObj.downloadResources +'</div>'
-		    +filesDiv+
-		    '	<div class="fileNameRow">'+
-		    '		<label for="zipFileName"> '+ langObj.downloadFileName +' </label>'+
-		    '		<input dojoType="dijit.form.TextBox" type="text" name="zipFileName" id="zipFileName" value="'+proposedFileName+'"></input><span>.zip</span>'+
-		    '	</div>'+
-		    '<div><button dojoType="dijit.form.Button" type="submit" >'+ langObj.downloadNow +'</button></div>' +
-		    "</div>" ;
-		this.dialog = new Dialog({
-			id: "downloadDialog",
-			title: langObj.titleDownload,
-			onCancel: function() {
-				this.destroyRecursive(false);
-			},
-			execute: dojo.hitch(this, "download")
-		});
-
-		this.dialog.setContent(formHtml);
-		this.dialog.show();
-
+		Workbench.showModal(new DownloadSelected(), "Download", "width: 400px");
 	},
 	
 	isEnabled: function(selection){
 		var files = uiResource.getSelectedResources();
 		return files && files.length>0;
-	},
-	
-	download: function(value){
-		var resources=dojo.map(this._files,function(item){return item.getPath();});
-		this.dialog.destroyRecursive(false);
-		var path=value.zipFileName;
-			
-		resource.download(resources, path + ".zip", Workbench.getProject());
 	}
 });
 });

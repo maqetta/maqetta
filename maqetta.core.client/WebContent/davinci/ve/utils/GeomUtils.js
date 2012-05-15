2bfb0f7faddc3a8c5d1d433210156dd3fcb8b442
JonFerraiolo <jferrai@us.ibm.com>
2012-05-15 14:44:02 -0700

    Fixes #2277 - dojo.position provides *transformed* locations for DOM nodes, not the raw box model locations. This caused visual editor problems in widgets that use CSS transform tricks. Instead, use offsetLeft/Top/Width/Height directly.

--- maqetta.core.client/WebContent/davinci/ve/utils/GeomUtils.js
+++ maqetta.core.client/WebContent/davinci/ve/utils/GeomUtils.js
@@ -18,8 +18,9 @@ return /** @scope davinci.ve.utils.GeomUtils */ {
 	 */
 	getMarginBoxPageCoords: function(node){
 		var MarginBoxPageCoords;
+		var userWin = node.ownerDocument.defaultView;
 		win.withDoc(node.ownerDocument, function(){
-			var BorderBoxPageCoords = domGeom.position(node, true);
+			var BorderBoxPageCoords = this.position(node);
 			var MarginExtents = domGeom.getMarginExtents(node);
 			MarginBoxPageCoords = {
 					l:BorderBoxPageCoords.x - MarginExtents.l,
@@ -27,8 +28,18 @@ return /** @scope davinci.ve.utils.GeomUtils */ {
 					w:BorderBoxPageCoords.w + MarginExtents.l + MarginExtents.r,
 					h:BorderBoxPageCoords.h + MarginExtents.t + MarginExtents.b
 			};
-		});
+		}.bind(this));
 		return MarginBoxPageCoords;
+	},
+	
+	/* Rewrite of Dojo's dom-geometry.position() to not use getBoundingClientRect()
+	 * which messes up Maqetta in presence of CSS3 transforms. Maqetta's calculations
+	 * are all based on CSS box model (margins, borders, padding, left/top)
+	 * not the actual screen locations resulting after applying transforms.
+	 */
+	position: function(/*DomNode*/node){
+		return {x: node.offsetLeft, y: node.offsetTop, w: node.offsetWidth, h: node.offsetHeight};
 	}
+
 };
 });

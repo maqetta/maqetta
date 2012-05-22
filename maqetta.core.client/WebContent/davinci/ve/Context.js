989160b53d51201e095796b53a271d28841a5be3
JonFerraiolo <jferrai@us.ibm.com>
2012-05-22 15:09:19 -0700

    (Follow-on to document.css fix) Move some startup logic into the appropriate deferred function so that doc is ready.

--- maqetta.core.client/WebContent/davinci/ve/Context.js
+++ maqetta.core.client/WebContent/davinci/ve/Context.js
@@ -1025,13 +1025,6 @@ return declare("davinci.ve.Context", [ThemeModifier], {
 					callback.call((scope || this), failureInfo);
 				}
 			}
-			// At doc load time, call the routine that makes document adjustments each time
-			// new widgets are added or widgets are deleted.
-			this.anyDojoxMobileWidgets = undefined;
-			this.widgetAddedOrDeleted();
-			
-			// pagebuilt event triggered after converting model into dom for visual page editor
-			dojo.publish('/davinci/ui/context/pagebuilt', [this]);
 		}
 	},
 

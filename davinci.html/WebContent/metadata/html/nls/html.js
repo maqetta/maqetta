1dee76824673f9383e2ebf70c52591e6721acfda
Tony Erwin <aerwin@us.ibm.com>
2012-05-22 17:19:57 -0400

    Remove media samples and update smart input for media elements (#2368 & #1761)
    We decided the media samples (for AUDIO and VIDEO elements) were too
    large, so we removed them and uploaded to maqetta.org.
    
    Then, I changed Smart Input for AUDIO, VIDEO, and EMBED to get rid of
    the file chooser and just offer a text field. This then essentially
    makes use of the same smart input UI as most other widgets (plus some
    customized logic).
    
    Finally, the AUDIO/VIDEO smart input was enabled for multiline with
    each line representing a different SOURCE element. This last part
    addresses concerns in #1761.

--- davinci.html/WebContent/metadata/html/nls/html.js
+++ davinci.html/WebContent/metadata/html/nls/html.js
@@ -7,7 +7,12 @@ define({ root: {
 		
 		//html/SelectInput.js
 		"selectInputHelp":"Enter multiple lines of text. Each line will correspond to an option item in the list. Indicate the default option to be selected with the \"+\" character at the start of the line. One level of nesting is supported. Indicate a nsted level by putting a \">\" character at the start of a line. ",
-			
+		
+		//html/MediaInput.js
+		"audioInputHelp":"Enter multiple lines of text. Each line should be the absolute or relative URL of an audio file. At runtime, the browser will choose which (if any) of the audio formats it can best support.",
+		"videoInputHelp":"Enter multiple lines of text. Each line should be the absolute or relative URL of an video file. At runtime, the browser will choose which (if any) of the video formats it can best support.",
+		"embedInputHelp":"Enter the absolute or relative URL for the content to displayed within the &lt:embed&gt; element. ",
+
 		//html/FieldsetInput.js
 		"fieldsetInputHelp":"Enter text to be used as the legend for the fieldset."
 }});

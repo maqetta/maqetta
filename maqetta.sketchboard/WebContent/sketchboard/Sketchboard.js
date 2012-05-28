277fe79175da3f12ff0038e0ca1a265206b6ab35
JonFerraiolo <jferrai@us.ibm.com>
2012-05-28 14:06:35 -0700

    Use StackContainer instead of TabContainer

--- maqetta.sketchboard/WebContent/sketchboard/Sketchboard.js
+++ maqetta.sketchboard/WebContent/sketchboard/Sketchboard.js
@@ -1,9 +1,9 @@
 define([
 		"dojo/_base/declare",
-		"dijit/layout/TabContainer"
-], function(declare, TabContainer, ContentPane){
+		"dijit/layout/StackContainer"
+], function(declare, StackContainer, ContentPane){
 	
-	return declare("sketchboard.Sketchboard", [TabContainer], {
+	return declare("sketchboard.Sketchboard", [StackContainer], {
 		
 		postCreate: function(){
 			debugger;

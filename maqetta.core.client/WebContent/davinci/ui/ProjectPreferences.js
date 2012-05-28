49829da30d2f7f54ddda41ab928438f23bd68a10
JonFerraiolo <jferrai@us.ibm.com>
2012-05-27 22:19:18 -0700

    Update project preferences for templates folder

--- maqetta.core.client/WebContent/davinci/ui/ProjectPreferences.js
+++ maqetta.core.client/WebContent/davinci/ui/ProjectPreferences.js
@@ -18,6 +18,7 @@ return declare("davinci.ui.ProjectPreferences", [ContentPane, TemplatedMixin], {
 
 	templateString: "<div><table style='margin: 4px;' cellspacing='4'><tbody>" +
 	"<tr><td>Web Content:</td><td><div dojoAttachPoint='webContentNode'></div></td></tr>" +
+	"<tr><td>Templates Folder:</td><td><div dojoAttachPoint='templatesFolderNode'></div></td></tr>" +
 	"<tr><td>Theme Folder:</td><td><div dojoAttachPoint='themeFolderNode'></div></td></tr>" +
 	"<tr><td>Widget Folder:</td><td><div dojoAttachPoint='widgetFolderNode'></div></td></tr>" +
 	"</tbody></table></div>",
@@ -29,6 +30,7 @@ return declare("davinci.ui.ProjectPreferences", [ContentPane, TemplatedMixin], {
 
 	postCreate: function(){
 		this._webContentNode = new TextBox({}, this.webContentNode);
+		this._templatesFolderNode = new TextBox({}, this.templatesFolderNode);
 		this._themeFolderNode = new TextBox({}, this.themeFolderNode);
 		this._widgetFolderNode = new TextBox({}, this.widgetFolderNode);
 		
@@ -52,17 +54,20 @@ return declare("davinci.ui.ProjectPreferences", [ContentPane, TemplatedMixin], {
 		preferences = preferences || {};
 		this._webContentNode.set('value', preferences['webContentFolder']);
 		this._themeFolderNode.set('value', preferences['themeFolder']);
+		this._templatesFolderNode.set('value', preferences['templatesFolder']);
 		this._widgetFolderNode.set('value', preferences['widgetFolder']);
 	},
 
 	getPreferences: function(){
 		
 		var themeFolderNode = this._themeFolderNode;
+		var templatesFolderNode = this._templatesFolderNode;
 		var webContentNode = this._webContentNode;
 		var widgetFolderNode = this._widgetFoldertNode;
 		
 		var preferences = {
 			themeFolder: dojo.attr(themeFolderNode, 'value'),
+			templatesFolder: dojo.attr(templatesFolderNode, 'value'),
 			webContentFolder: dojo.attr(webContentNode, 'value'),
 			widgetFolder: dojo.attr(widgetFolderNode, 'value')
 		};

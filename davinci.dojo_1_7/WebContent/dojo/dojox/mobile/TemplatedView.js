a0ce5e570937aaf2168000d2d929efb0b9082758
Javier Pedemonte <javier@pedemonte.us>
2012-06-17 00:25:37 -0500

    Attempt at templated widget which inherits from dojox.mobile.View.

---
+++ davinci.dojo_1_7/WebContent/dojo/dojox/mobile/TemplatedView.js
@@ -0,0 +1,46 @@
+//>>built
+define([
+	"dojo/_base/declare",
+	"dojo/dom-construct",
+	"dojox/mobile/SwapView",
+	"dojox/mobile/parser",
+//	"dijit/_TemplatedMixin",
+//	"dijit/_WidgetsInTemplateMixin",
+	"dojo/text!./TemplatedView.html",
+	// following are widgets in template
+	"dojox/mobile/Button"
+], function(
+	declare,
+	domConstruct,
+	SwapView,
+	parser,
+//	_TemplatedMixin,
+//	_WidgetsInTemplateMixin,
+	template
+){
+
+	return declare("dojox.mobile.TemplatedView", [SwapView], {
+
+		templateString: template,
+
+		buildRendering: function(){
+			this.inherited(arguments);
+
+			// create DOM from template
+			var node = domConstruct.toDom(template);
+			if (node.nodeType !== 1) {
+				throw new Error('Invalid template: ' + template);
+			}
+
+			// parse any widgets in template
+			parser.parse(node);
+
+			// add DOM elements from template to existing container node
+			var dest = this.containerNode;
+			while (node.hasChildNodes()) {
+				dest.appendChild(node.firstChild);
+			}
+		}
+
+	});
+});

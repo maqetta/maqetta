b4ccb5eb7036c013dc34c910223630ac9baf46df
Javier Pedemonte <javier@pedemonte.us>
2012-07-11 18:36:37 -0500

    Remove `getDraggables` from Helper.js "skeleton".
    It's only relevant to the Shapes library, so moved it there.
    Also removed _TextHelperMixin.js, since it is no longer necessary.

--- maqetta.core.client/WebContent/davinci/skeletons/Helper.js
+++ maqetta.core.client/WebContent/davinci/skeletons/Helper.js
@@ -263,16 +263,6 @@ define(function() {
 //			return data;
 		},
 
-		/*
-		 * Special hooks for shape widgets.
-		 * Returns list of draggable end points for this shape in "px" units
-		 * relative to top/left corner of enclosing SPAN
-		 * @return {object} whose properties represent widget-specific types of draggable points
-		 *   For example, widgets that represent a series of points will include a 'points'
-		 *   property which is an array of object of the form {x:<number>,y:<number>}
-		 */
-		getDraggables: function(){},
-
 		/**
 		 * [getMarginBoxPageCoords description]
 		 * @param  {davinci/ve/_Widget} widget [description]

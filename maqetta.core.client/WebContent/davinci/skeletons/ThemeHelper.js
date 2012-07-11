8db3a53b21dcfd33e2443fea4815621bd994ce18
Javier Pedemonte <javier@pedemonte.us>
2012-07-11 14:49:11 -0500

    Add ThemeHelper.js skeleton.

---
+++ maqetta.core.client/WebContent/davinci/skeletons/ThemeHelper.js
@@ -0,0 +1,38 @@
+/**
+ * THIS FILE IS FOR REFERENCE/DOCUMENTATION ONLY.  Do not use directly, but feel
+ * free to copy for use, update and strip out the unnecessary functions.
+ */
+
+define(function() {
+
+	return {
+
+		/**
+		 * [getHeadStyleString description]
+		 * @return {String} [description]
+		 */
+		getHeadStyleString: function() {},
+
+		/**
+		 * [preThemeConfig description]
+		 * @param  {davinci/ve/Context} context [description]
+		 */
+		preThemeConfig: function(context) {},
+
+		/**
+		 * [getHeadImports description]
+		 * @param  {davinci/Theme} theme [description]
+		 * @return {String}       [description]
+		 */
+		getHeadImports: function(theme) {},
+
+		/**
+		 * [onContentChange description]
+		 * @param  {davinci/ve/Context} context [description]
+		 * @param  {davinci/Theme} theme   [description]
+		 */
+		onContentChange: function(context, theme) {}
+
+	};
+
+});

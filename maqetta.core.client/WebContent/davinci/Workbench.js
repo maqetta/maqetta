df25678c00be8c0aa9a886b357ed190e225dc580
JonFerraiolo <jferrai@us.ibm.com>
2012-08-06 13:24:09 -0700

    Fixes #2870 - now when posting a context menu, we temporarily add an invisible DIV that covers entire app. This captures mouse events so that the page editor doesn't attempt to select the widget under the click-away point.

--- maqetta.core.client/WebContent/davinci/Workbench.js
+++ maqetta.core.client/WebContent/davinci/Workbench.js
@@ -2194,6 +2194,11 @@ var PopupMenu = declare(Menu, {
 		this.menuOpened(event);
 		var open;
 		try{
+			// Create a DIV that will overlay entire app and capture events that might go to interior iframes
+			var menuOverlayDiv = document.getElementById('menuOverlayDiv');
+			if(!menuOverlayDiv){
+				menuOverlayDiv = dojo.create('div', {id:'menuOverlayDiv', style:'left:0px; top:0px; width:100%; height:100%; position:absolute; z-index:10;'}, document.body);
+			}
 			if(this.adjustPosition){
 				var offsetPosition=this.adjustPosition(event);
 					open = dijit.popup.open;
@@ -2203,6 +2208,12 @@ var PopupMenu = declare(Menu, {
 						open.call(dijit.popup, args);
 					};
 			}
+			this.onClose = function(){
+				var menuOverlayDiv = document.getElementById('menuOverlayDiv');
+				if(menuOverlayDiv){
+					menuOverlayDiv.parentNode.removeChild(menuOverlayDiv);
+				}
+			}.bind(this);
 			this.inherited(arguments);
 		}finally{
 			if(open){

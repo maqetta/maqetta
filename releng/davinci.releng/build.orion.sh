7a3d4a1d58bed8906194cf9d1951695302f8de48
bradley childs <childsb@us.ibm.com>
2012-08-16 15:38:45 -0500

    Revert "roll back baseDirectory removal. keeping for 1.0"
    This reverts commit d5c6f14f527c1de1b594a00a0111f7d2baf5d359.

--- releng/davinci.releng/build.orion.sh
+++ releng/davinci.releng/build.orion.sh
@@ -97,8 +97,8 @@ updateBaseBuilder () {
 	cd ${supportDir}
     if [ ! -d org.eclipse.releng.basebuilder_${basebuilderBranch} ]; then
         echo "[start - `date +%H\:%M\:%S`] Get org.eclipse.releng.basebuilder_${basebuilderBranch}"
-        #cmd="cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse $quietCVS ex -r $basebuilderBranch -d org.eclipse.releng.basebuilder_${basebuilderBranch} org.eclipse.releng.basebuilder"
-        cmd="git clone git://dev.eclipse.org/org.eclipse.releng/org.eclipse.releng.basebuilder.git -b ${basebuilderBranch} org.eclipse.releng.basebuilder_${basebuilderBranch}"
+        cmd="cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse $quietCVS ex -r $basebuilderBranch -d org.eclipse.releng.basebuilder_${basebuilderBranch} org.eclipse.releng.basebuilder"
+        #cmd="git clone git://dev.eclipse.org/org.eclipse.releng/org.eclipse.releng.basebuilder.git -b ${basebuilderBranch} org.eclipse.releng.basebuilder_${basebuilderBranch}"
         echo $cmd
         $cmd
         echo "[finish - `date +%H\:%M\:%S`] Done getting org.eclipse.releng.basebuilder_${basebuilderBranch}"

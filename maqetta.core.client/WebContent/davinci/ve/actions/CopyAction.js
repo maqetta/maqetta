b99ac9d55c77b877b18fa021bc132ad460c8fbc3
Tony Erwin <aerwin@us.ibm.com>
2012-08-30 00:53:42 -0500

    Fix copy of data store based widgets within same page (#3172)
    In the last few weeks, I had fixed an issue cutting and pasting across
    different pages. I failed to put the CutAction logic into CopyAction.
    With this commit, I created a new base class that both CutAction
    and CopyAction inherit from.

--- maqetta.core.client/WebContent/davinci/ve/actions/CopyAction.js
+++ maqetta.core.client/WebContent/davinci/ve/actions/CopyAction.js
@@ -1,64 +1,24 @@
 define([
     	"dojo/_base/declare",
     	"davinci/Workbench",
-    	"davinci/ve/actions/ContextAction",
+    	"./_CutCopyAction",
     	"davinci/commands/CompoundCommand",
     	"davinci/ve/commands/RemoveCommand"
-], function(declare, Workbench, ContextAction, CompoundCommand, RemoveCommand){
+], function(declare, Workbench, _CutCopyAction, CompoundCommand, RemoveCommand){
 
 
-return declare("davinci.ve.actions.CopyAction", [ContextAction], {
+return declare("davinci.ve.actions.CopyAction", [_CutCopyAction], {
 
-
-	run: function(context){
-		context = this.fixupContext(context);
-		if(context){
-			if (context.declaredClass=="davinci.ve.PageEditor" && context._displayMode=="source")
-			{
-				context.htmlEditor.copyAction.run();
-				return;
-			}
-			var selection = this._normalizeSelection(context);
-			if(selection.length > 0){
-				var command = new CompoundCommand();
-				var data = [];
-				var reorderedSelection = context.reorderPreserveSiblingOrder(selection);
-				dojo.forEach(reorderedSelection, function(w){
-					//TODO: GENERALIZE THIS
-					var d = w.getData(  {identify: false});
-					if(d){
-						data.push(d);
-					}
-				});
-				var oldData = davinci.Runtime.clipboard;
-				davinci.Runtime.clipboard=data;
-				if(!oldData){
-					context.onSelectionChange(selection); // force to enable Paste action
-				}
-			}
-		}
+	_invokeSourceEditorAction: function(context) {
+		context.htmlEditor.copyAction.run();
 	},
-
-	isEnabled: function(context){
-		context = this.fixupContext(context);
-		var e = Workbench.getOpenEditor();
-		if (e && context) {
-			var anySelection = (context.getSelection().length > 0);
-			if(e.declaredClass == 'davinci.ve.PageEditor'){
-				var displayMode = e.getDisplayMode();
-				return anySelection && displayMode != 'source';
-			}else{
-				return anySelection;
-			}
-		}else{
-			return false;
+	
+	_executeAction: function(context, selection, data, removeCommand) {
+		var oldData = davinci.Runtime.clipboard;
+		davinci.Runtime.clipboard=data;
+		if(!oldData){
+			context.onSelectionChange(selection); // force to enable Paste action
 		}
-	},
-
-	shouldShow: function(context){
-		context = this.fixupContext(context);
-		var editor = context ? context.editor : null;
-		return (editor && editor.declaredClass == 'davinci.ve.PageEditor');
 	}
 });
 });
---
+++ maqetta.core.client/WebContent/davinci/ve/actions/_CutCopyAction.js
@@ -0,0 +1,99 @@
+define([
+    	"dojo/_base/declare",
+    	"davinci/Workbench",
+    	"davinci/ve/actions/ContextAction",
+    	"davinci/commands/CompoundCommand",
+    	"davinci/ve/commands/RemoveCommand",
+    	"davinci/ve/widget"
+], function(declare, Workbench,ContextAction, CompoundCommand, RemoveCommand, Widget){
+
+
+return declare("davinci.ve.actions.CutAction", [ContextAction], {
+
+	run: function(context){
+		context = this.fixupContext(context);
+		if(context){
+		    if (context.declaredClass=="davinci.ve.PageEditor" && context._displayMode=="source")
+		    {
+		    	this._invokeSourceEditorAction(context);
+		    	return;
+		    }
+			var selection = this._normalizeSelection(context);
+			if(selection.length > 0){
+				var command = new CompoundCommand();
+				var data = [];
+				var reorderedSelection = context.reorderPreserveSiblingOrder(selection);
+				dojo.forEach(reorderedSelection, function(w){
+					var d = w.getData( {identify: false});
+					if(d){
+						data.push(d);
+					}
+					var helper = w.getHelper();
+					var c;
+					if(helper && helper.getRemoveCommand) {
+						c = helper.getRemoveCommand(w);
+						
+						// Look for associated widgets in compound command (for things like
+						// grids, trees, etc.) and add that as a special field to the widget
+						// data.
+						if (c.name === "compound") {
+							// Let's loop (backwards) through the sub commands and 
+							// get the data for the widgets being deleted (skipping the
+							// very last one which by convention should be the widget
+							// itself).
+							var associatedCopiedWidgetData = [];
+							var commands = c._commands;
+							for (var i = commands.length - 1; i > 0; i--) {
+								var subCommand = commands[i];
+								if (subCommand.name === "remove") {
+									var subCommandWidget = Widget.byId(subCommand._id);
+									var subCommandWidgetData = subCommandWidget.getData( {identify: false});
+									associatedCopiedWidgetData.push(subCommandWidgetData);
+								}
+							}
+							d.associatedCopiedWidgetData = associatedCopiedWidgetData;
+						}
+						
+					} else {
+						c = new RemoveCommand(w);
+					}
+					command.add(c /*new RemoveCommand(w)*/);
+				});
+
+				// Execute the copy or paste action (delegated to subclass)
+				this._executeAction(context, selection, data, command);
+			}
+		}
+	},
+
+	isEnabled: function(context){
+		context = this.fixupContext(context);
+		var e = Workbench.getOpenEditor();
+		if (e && context) {
+			var anySelection = (context.getSelection().length > 0);
+			if(e.declaredClass == 'davinci.ve.PageEditor'){
+				var displayMode = e.getDisplayMode();
+				return anySelection && displayMode != 'source';
+			}else{
+				return anySelection;
+			}
+		}else{
+			return false;
+		}
+	},
+
+	shouldShow: function(context){
+		context = this.fixupContext(context);
+		var editor = context ? context.editor : null;
+		return (editor && editor.declaredClass == 'davinci.ve.PageEditor');
+	},
+	
+	_invokeSourceEditorAction: function(context) {
+		// Subclass should overrride
+	},
+	
+	_executeAction: function(context, selection, data, removeCommand) {
+		// Subclass should overrride
+	}
+});
+});

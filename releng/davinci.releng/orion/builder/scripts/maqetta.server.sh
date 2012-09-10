51929c7c5b0fa9f008d5000f0101ed52d1ff2153
Javier Pedemonte <javier@pedemonte.us>
2012-09-09 20:15:18 -0500

    Change launch scripts to read from config file.
    Fixes issue where params weren't getting passed to 
    Java process.
    #3302

--- releng/davinci.releng/orion/builder/scripts/maqetta.server.sh
+++ releng/davinci.releng/orion/builder/scripts/maqetta.server.sh
@@ -1,91 +1,128 @@
-#!/bin/sh
+#!/bin/bash
 
 print_help() {
-    echo "Usage: ./maqetta.server.mac.command [OPTION]..."
-    echo ""
-    echo "Options:"
-    echo " -p, --port <port>           server listens on port # <port>; defaults to 50000"
-    echo " -m, --smtpServer <hostname> provide the hostname of an SMTP server; defaults to localhost"
-    echo " -c, --consolePort <port>    enable console, listening on port number <port>"
-    echo " --webBuilder <URL>          enable Dojo Web Builder, using given URL"
-    echo " -h, --help                  show this message"
+	echo "Usage: ./maqetta.server.mac.command"
+	echo ""
+	echo "Options:"
+	echo "  -h, --help                  show this message"
+	echo ""
+	echo " => Further options are read from 'maqetta.conf' file."
 }
 
-# defaults
-port=50000
-consolePort="-console"
-smtpServer="localhost"
-
-# parse options
 while [ "${1+isset}" ]; do
-    case "$1" in
-        -p|--port)
-            port=$2
-            shift 2
-            ;;
-        -m|--smtpServer)
-            smtpServer=$2
-            shift 2
-            ;;
-        -c|--consolePort)
-            consolePort="-console $2"
-            shift 2
-            ;;
-        --webBuilder)
-            dwbUrl="-Dmaqetta.dojoWebBuilder=$2"
-            shift 2
-            ;;
-        -h|--help)
-            print_help
-            exit
-            ;;
-        *)
-            echo "Error: Unknown option: $1" >&2
-            exit 1
-            ;;
-    esac
+	case "$1" in
+		-h|--help)
+			print_help
+			exit
+			;;
+		*)
+			echo "Error: Unknown option: $1" >&2
+			exit 1
+			;;
+	esac
 done
 
-# Make sure user has Java 1.5 installed
-javaversion=`java -version 2>&1 | grep "java version"`
-if [ -z "$javaversion" ]; then
-	echo Unable to determine which version of Java is installed on this machine
-else
-	majorversionnumber=`echo "$javaversion" | sed 's/^[^0-9]*\([0-9][0-9]*\)\..*$/\1/'`
-	minorversionnumber=`echo "$javaversion" | sed 's/^[^0-9]*[0-9][0-9]*\.\([0-9][0-9]*\).*$/\1/'`
-	echo Java version=$majorversionnumber.$minorversionnumber
-	if [[ $majorversionnumber -lt 1 || $minorversionnumber -lt 5 ]]; then
-		echo
-		echo !!!!!!!!!!!!!!
-		echo CANNOT LAUNCH MAQETTA SERVER - Maqetta requires Java 1.5.
-		echo	
-		echo !!!!!!!!!!!!!!
-		echo
-		exit
+#
+# Check minimum Java version. Must be >= 1.5
+#
+check_java() {
+	javaversion=`java -version 2>&1 | grep "java version"`
+	if [ -z "$javaversion" ]; then
+		echo Unable to determine which version of Java is installed on this machine
+	else
+		majorversionnumber=`echo "$javaversion" | sed 's/^[^0-9]*\([0-9][0-9]*\)\..*$/\1/'`
+		minorversionnumber=`echo "$javaversion" | sed 's/^[^0-9]*[0-9][0-9]*\.\([0-9][0-9]*\).*$/\1/'`
+		echo Java version=$majorversionnumber.$minorversionnumber
+		if [[ $majorversionnumber -lt 1 || $minorversionnumber -lt 5 ]]; then
+			echo "Error: Maqetta requires Java 1.5"
+			exit 1
+		fi
 	fi
-fi
+}
 
-echo
-echo !!!!!!!!!!!!!!
-echo NOTE: CLOSING THIS WINDOW WILL 
-echo       STOP THE MAQETTA SERVER PROCESS
-echo !!!!!!!!!!!!!
-echo
-scriptdir=`dirname "$0"`
-# usersdir="$HOME/Library/Application Support/maqetta/users"
-usersdir="$scriptdir"/users
-
-# Convert to absolute file name
-D=`dirname "$usersdir"`
-B=`basename "$usersdir"`
-
-jarFilePath=`ls "$scriptdir"/plugins/org.eclipse.equinox.launcher*.jar`
-echo Using jar: "$jarFilePath"
-
-absusersdir="`cd \"$D\" 2>/dev/null && pwd || echo \"$D\"`/$B"
-echo Using directory: "$absusersdir"
-
-echo Start your browser at: http://localhost:$port/maqetta
-mkdir -p "$absusersdir"
+MAQ_BASE=`dirname "$0"`
+JAVA_ARGS=""
 # XXX Issue 2941 - Need to specify "-clean" flag due to possible Eclipse bug.
-java -Dorion.core.configFile=${scriptdir}/maqetta.conf "-Dmaqetta.baseDirectory=$absusersdir" -Dorg.eclipse.equinox.http.jetty.http.port=$port $dwbUrl -jar "$jarFilePath" $consolePort -noExit -clean -data $absusersdir
+APP_ARGS="-console -noExit -clean"
+
+#
+# Function which reads in configuration options
+#
+read_conf()
+{
+	# check for configuration file
+	MAQ_CONFIG=maqetta.conf
+	test -r $MAQ_CONFIG || { echo "$MAQ_CONFIG not found"; exit 1; }
+
+	# Some props (such as 'admin' password) **must** be in a config file; passing
+	# them on the command line won't work.
+	JAVA_ARGS="${JAVA_ARGS} -Dorion.core.configFile=${MAQ_CONFIG}"
+
+	# read config
+	while read line
+	do
+		key=${line%%=*}
+		val=${line#*=}
+
+		case "$key" in
+			"maqetta.baseDirectory")
+				base_dir=$val
+				# also pass in as "-data" property
+				APP_ARGS="${APP_ARGS} -data ${val}"
+				;;
+			"maqetta.extra_java_args")
+				extra_java_args=$val
+				continue	# don't add to JAVA_ARGS
+				;;
+			"org.eclipse.equinox.http.jetty.http.port")
+				port=$val
+				;;
+		esac
+
+		JAVA_ARGS="${JAVA_ARGS} -D${line}"
+
+	done < <(grep -v "^#" ${MAQ_CONFIG} | grep -v "^\s*$")
+
+	# get jar path
+	jarFilePath=`ls "$MAQ_BASE"/plugins/org.eclipse.equinox.launcher*.jar`
+	JAR_FILE="-jar ${jarFilePath}"
+}
+
+do_start() {
+	echo
+	echo !!!!!!!!!!!!!!
+	echo "NOTE: CLOSING THIS WINDOW WILL "
+	echo "      STOP THE MAQETTA SERVER PROCESS"
+	echo !!!!!!!!!!!!!
+	echo
+
+	if [ -z $base_dir ]; then
+		base_dir="$MAQ_BASE"/users
+		# Convert to absolute file name
+		D=`dirname "$base_dir"`
+		B=`basename "$base_dir"`
+		base_dir="`cd \"$D\" 2>/dev/null && pwd || echo \"$D\"`/$B"
+		# set default value
+		JAVA_ARGS="${JAVA_ARGS} -Dmaqetta.baseDirectory=${base_dir}"
+		APP_ARGS="${APP_ARGS} -data ${base_dir}"
+	fi
+	mkdir -p "$base_dir"
+	echo "Using directory: ${base_dir}"
+
+	if [ -z $port ]; then
+		# defaults to 8080
+		port=8080
+	fi
+	echo "Start your browser at: http://localhost:$port/maqetta"
+
+	echo
+	echo "Type \"exit\" or Ctrl-C to stop the server."
+	echo
+
+	java $JAVA_ARGS ${extra_java_args} $JAR_FILE $APP_ARGS
+}
+
+
+check_java
+read_conf
+do_start

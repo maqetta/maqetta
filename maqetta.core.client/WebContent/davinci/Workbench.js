maqetta/maqetta

commit f5052b7c75e4029ad92b45187f730e393e222e7b

Maybe takes care of #3279 - null cs exception when opening multiple pages at once.

maqetta.core.client/WebContent/davinci/Workbench.js
@@ -1721,6 +1721,7 @@ var Workbench = {
 	_switchEditor: function(newEditor, startup) {
 		var oldEditor = Runtime.currentEditor;
 		Runtime.currentEditor = newEditor;
+		Workbench._state.activeEditor=newEditor ? newEditor.fileName : null;
 		this._showEditorTopPanes();
 		try {
 			dojo.publish("/davinci/ui/editorSelected", [{
@@ -1731,8 +1732,6 @@ var Workbench = {
 			console.error(ex);
 		}
 		Workbench._updateTitle(newEditor);
-	
-		Workbench._state.activeEditor=newEditor ? newEditor.fileName : null;
 		
 		setTimeout(function(){
 			// kludge: if there is a visualeditor and it is already populated, resize to make Dijit visualEditor contents resize

maqetta.core.client/WebContent/davinci/workbench/EditorContainer.js
@@ -81,7 +81,7 @@ return declare(ToolbaredContainer, {
 					// but also avoid problems with display:none on hidden tabs making it impossible
 					// to do geometry measurements in editor initialization
 					var editorsContainer = "editors_container";
-					if(dijit.byId(editorsContainer).selectedChildWidget.domNode == this.domNode){
+					if(davinci.Workbench._state.activeEditor == fileName){
 						// Tab is visible.  Go ahead
 						editor.setContent(fileName, content, newHtmlParams);
 

commit 39697470face2a3f6a3887273e7b07db0d84cb3b

Fix regression caused by f5052b7c75e4029ad92b45187f730e393e222e7b, which was the fix for #3279.

maqetta.core.client/WebContent/davinci/Workbench.js
@@ -235,7 +235,8 @@ var initializeWorkbenchState = function(){
 								content: resource.getContentSync(),
 								noSelect: noSelect, // style: should flip logic to use "select" property
 								isDirty: resource.isDirty(),
-								startup: false
+								startup: false,
+								initializationTime: true
 							});
 //						});
 					}
@@ -1499,6 +1500,13 @@ var Workbench = {
 		
 		if (!keywordArgs.noSelect) {
 			editorsContainer.selectChild(editorContainer);
+		}
+		//FIXME: this is very kludgy. At initialization time, we want EditorContainer.js 
+		//to filter past all editors except the current filename to prevent the cs=null issue #3279.
+		//But when not at initialization time, we need to make sure the
+		//current activeEditor is set to the "fileName".
+		if(!keywordArgs.initializationTime){
+			Workbench._state.activeEditor = fileName;
 		}
 		editorContainer.setEditor(editorExtension, fileName, content, keywordArgs.fileName, editorContainer.domNode, newHtmlParams).then(function(editor) {
 			if (keywordArgs.startLine) {
da13d4be5cfa9eaa7ad6183d71eb795c7e3ae8ce
JonFerraiolo <jferrai@us.ibm.com>
2012-09-30 10:22:13 -0700

    Fixes #396 - make surround action work well with absolutely positioned widgets.

--- maqetta.core.client/WebContent/davinci/ve/actions/SurroundAction.js
+++ maqetta.core.client/WebContent/davinci/ve/actions/SurroundAction.js
@@ -1,12 +1,16 @@
 define([
     	"dojo/_base/declare",
+    	"dojo/_base/array",
     	"davinci/ve/actions/ContextAction",
     	"davinci/commands/CompoundCommand",
-    	"davinci/ve/commands/RemoveCommand",
     	"davinci/ve/commands/AddCommand",
+    	"davinci/ve/commands/StyleCommand",
+    	"davinci/ve/commands/MoveCommand",
+    	"davinci/ve/commands/ResizeCommand",
     	"davinci/ve/commands/ReparentCommand",
-    	"davinci/ve/widget"
-], function(declare, ContextAction, CompoundCommand, RemoveCommand, AddCommand, ReparentCommand, Widget){
+    	"davinci/ve/widget",
+    	"davinci/ve/utils/GeomUtils"
+], function(declare, Array, ContextAction, CompoundCommand, AddCommand, StyleCommand, MoveCommand, ResizeCommand, ReparentCommand, Widget, GeomUtils){
 
 
 return declare("davinci.ve.actions.SurroundAction", [ContextAction], {
@@ -14,6 +18,8 @@ return declare("davinci.ve.actions.SurroundAction", [ContextAction], {
 
 	run: function(context){
 		context = this.fixupContext(context);
+		var minX, minY, maxX, maxY;
+		var marginBoxPageCoords = [];
 		var newWidget, tag = this.item.surroundWithTagName;
 		if(!tag){
 			console.error('missing surroundWithTagName');
@@ -25,16 +31,69 @@ return declare("davinci.ve.actions.SurroundAction", [ContextAction], {
 		var command = new CompoundCommand(),
 			selection = [].concat(context.getSelection()),
 			first = selection[0],
-			parent = first.getParent();
-
+			parent = first.getParent(),
+			parentMarginBoxPageCoords = GeomUtils.getMarginBoxPageCoords(parent.domNode);
 		selection.sort(function(a, b){
 			return parent.indexOf(a) - parent.indexOf(b);
 		});
+		var userdoc = context.getDocument();	// inner document = user's document
+		var userDojo = userdoc.defaultView && userdoc.defaultView.dojo;
+		var allAbsolute = Array.every(selection, function(widget){
+			if(userDojo){
+				position_prop = userDojo.style(widget.domNode, 'position');
+				return (position_prop == 'absolute');
+			}
+		});
+		if(allAbsolute){
+			Array.forEach(selection, function(w, i){
+				// Store away margin boxes for later use
+				marginBoxPageCoords.push(GeomUtils.getMarginBoxPageCoordsCached(w.domNode));
+				
+				// Add a seemingly superfluous MoveCommand at front of command stack
+				// so that when an Undo occurs, the surrounded widgets will properly
+				// go back to original correct location
+				command.add(new MoveCommand(w, marginBoxPageCoords[i].l, marginBoxPageCoords[i].t, null, marginBoxPageCoords[i]));
+			});
+		}
 		command.add(new AddCommand(newWidget, parent, parent.indexOf(first)));
-		dojo.forEach(selection, function(w){
+		if(allAbsolute){
+			command.add(new StyleCommand(newWidget, [{'position':'absolute'}]));
+			Array.forEach(selection, function(w, i){
+				var r = marginBoxPageCoords[i].l + marginBoxPageCoords[i].w;
+				var b = marginBoxPageCoords[i].t + marginBoxPageCoords[i].h;
+				if(i == 0 || marginBoxPageCoords[i].l < minX){
+					minX = marginBoxPageCoords[i].l;
+				}
+				if(i == 0 || marginBoxPageCoords[i].l < minY){
+					minY = marginBoxPageCoords[i].t;
+				}
+				if(i == 0 || r > maxX){
+					maxX = r;
+				}
+				if(i == 0 || b > maxY){
+					maxY = b;
+				}
+			});
+			command.add(new MoveCommand(newWidget, minX, minY));
+			command.add(new ResizeCommand(newWidget, maxX-minX, maxY-minY));
+		}
+		Array.forEach(selection, function(w){
 			command.add(new ReparentCommand(w, newWidget, "last"));
 		});
+		if(allAbsolute){
+			// Need to perform MoveCommand on selected widgets after Reparent command
+			// because MoveCommand accepts page-relative coordinates and then
+			// adjusts left/top properties to be relative to widget's offsetParent
+			Array.forEach(selection, function(w, i){
+				command.add(new MoveCommand(w, 
+						marginBoxPageCoords[i].l, 
+						marginBoxPageCoords[i].t, 
+						null,
+						marginBoxPageCoords[i]));
+			});
+		}
 		context.getCommandStack().execute(command);
+		context.select(newWidget);
 	},
 
 	isEnabled: function(context){
@@ -45,7 +104,7 @@ return declare("davinci.ve.actions.SurroundAction", [ContextAction], {
 			if(!parent){
 				return false;
 			}
-			var siblings = dojo.every(context.getSelection(), function(selection){
+			var siblings = Array.every(context.getSelection(), function(selection){
 				var selectionParent = selection.getParent();
 				if(!selectionParent){
 					return false;

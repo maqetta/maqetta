7782859d066822c1a84c8f63aebd79c766d619b8
Bill Reed <billreed@us.ibm.com>
2012-11-02 14:37:32 -0400

    #3561 better code

--- maqetta.core.client/WebContent/davinci/html/CSSFile.js
+++ maqetta.core.client/WebContent/davinci/html/CSSFile.js
@@ -94,11 +94,10 @@ return declare("davinci.html.CSSFile", CSSElement, {
 	getText: function(context) {
 		context = context || {};
 		context.indent = 0;
-		var s = "";
-		for ( var i = 0; i < this.children.length; i++ ) {
-			s = s + this.children[i].getText(context);
-		}
-		return s;
+
+		return this.children.map(function(child) {
+			return child.getText(context);
+		}).join("");
 	},
 
 	getCSSFile: function() {

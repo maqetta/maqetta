0d0b172a90413eae4d827091b32f31ec58e65ac6
Adam Peller <peller@Adams-MacBook-Pro.local>
2012-11-05 14:24:29 -0500

    cherry-pick b1610d20fd8e417bb95fbf267e7901137dd19941 to replace Orion's i18n bundle with Dojo's format

--- maqetta.core.client/WebContent/orion/editor/nls/messages.js
+++ maqetta.core.client/WebContent/orion/editor/nls/messages.js
@@ -13,14 +13,38 @@
 
 /*global define*/
 
-define(['orion/textview/i18n!orion/editor/nls/messages'], function(bundle) {
-	var result = {
-		root: true
-	};
-	Object.keys(bundle).forEach(function(key) {
-		if (typeof result[key] === 'undefined') {
-			result[key] = bundle[key];
-		}
-	});
-	return result;
+define({
+	root: {
+		"Comment": "Comment",
+		"Flat outline": "Flat outline",
+		"incrementalFind": "Incremental find: ${0}",
+		"incrementalFindNotFound": "Incremental find: ${0} (not found)",
+		"find": "Find...",
+		"undo": "Undo",
+		"redo": "Redo",
+		"cancelMode": "Cancel Current Mode",
+		"findNext": "Find Next Occurrence",
+		"findPrevious": "Find Previous Occurrence",
+		"incrementalFindKey": "Incremental Find",
+		"indentLines": "Indent Lines",
+		"unindentLines": "Unindent Lines",
+		"moveLinesUp": "Move Lines Up",
+		"moveLinesDown": "Move Lines Down",
+		"copyLinesUp": "Copy Lines Up",
+		"copyLinesDown": "Copy Lines Down",
+		"deleteLines": "Delete Lines",
+		"gotoLine": "Goto Line...",
+		"gotoLinePrompty": "Goto Line:",
+		"nextAnnotation": "Next Annotation",
+		"prevAnnotation": "Previous Annotation",
+		"lastEdit": "Last Edit Location",
+		"toggleLineComment": "Toggle Line Comment",
+		"addBlockComment": "Add Block Comment",
+		"removeBlockComment": "Remove Block Comment",
+		"linkedModeEntered": "Linked Mode entered",
+		"linkedModeExited": "Linked Mode exited",
+		"syntaxError": "Syntax Error",
+		"contentAssist": "Content Assist",
+		"lineColumn": "Line ${0} : Col ${1}"
+	}
 });

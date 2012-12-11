7128121cd2445e8c050eee7dede2d14aaae7667b
JonFerraiolo <jferrai@us.ibm.com>
2012-12-11 08:59:36 -0800

    For mobile ListItem, update logic to prevent user clicks in the designer from activating a transition. Dojo 1.8 offers different (and better hooks) for intercepting click events. Using the new hooks now.

--- davinci.dojo_1_8/WebContent/metadata/dojox/mobile/ListItemHelper.js
+++ davinci.dojo_1_8/WebContent/metadata/dojox/mobile/ListItemHelper.js
@@ -9,20 +9,20 @@ ListItemHelper.prototype = {
 
 	create: function(widget, srcElement) {
 		var dijitWidget = widget.dijitWidget;
-		if(dijitWidget && dijitWidget.anchorNode){
-			// Fix for #705.
-			// The ListItem widget's startup logic registers an onclick
-			// handler, and if the 'moveTo' property has a reference to a view,
-			// then this built-in onclick handler will launch an animated
-			// transition to make that view visible. This is good for runtime execution,
-			// but we don't want this onclick handler to execute in the page editor.
-			// So, register a "click" handler in the capture phase (happens before default bubble phase)
-			// that calls stopPropagation(), which prevents the ListItem's onclick logic from getting invoked.
-			// This allows event to bubble up to ancestor widgets, and therefore
-			// will be caught by Maqetta and will cause a selection action to occur.
-			dijitWidget.anchorNode.addEventListener("click",function(e){
-				e.stopPropagation();		
-			}, true);
+		// Fix for #705.
+		// The ListItem widget's startup logic registers an onclick
+		// handler, and if the 'moveTo' property has a reference to a view,
+		// then this built-in onclick handler will launch an animated
+		// transition to make that view visible. This is good for runtime execution,
+		// but we don't want this onclick handler to execute in the page editor.
+		// So, register a onClick function that returns false, which causes
+		// the ListItem widget to forgo its normal processing.
+		// But the click event will bubble up to ancestor widgets, and therefore
+		// will be caught by Maqetta and will cause a selection action to occur.
+		if(dijitWidget){
+			dijitWidget.onClick = function(){
+				return false;
+			}
 		}
 	},
 

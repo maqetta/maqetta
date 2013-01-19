1966ef70646be6497129189ff9b17f5ac85660e6
JonFerraiolo <jferrai@us.ibm.com>
2013-01-19 15:03:51 -0800

    Migrate shapes.Circle widgets from M8 to M9

--- maqetta.shapes/WebContent/shapes/_CircleMixin.js
+++ maqetta.shapes/WebContent/shapes/_CircleMixin.js
@@ -15,28 +15,26 @@ define([
 		
 		_updateBorderRadius: function(){
 			var domNode = this.domNode;
-			if(domNode){
-				var bw, rx, ry;
-				var bwString = domStyle.get(domNode, 'borderWidth');
-				if(bwString){
-					bw = parseFloat(bwString);
-				}
-				var w = domNode.offsetWidth;
-				var h = domNode.offsetHeight;
-				if(isNaN(bw) || bw < 0 || w <= 0 || h <= 0){
-					domNode.style.borderTopLeftRadius = '0px';
-					domNode.style.borderTopRightRadius = '0px';
-					domNode.style.borderBottomLeftRadius = '0px';
-					domNode.style.borderBottomRightRadius = '0px';
-				}else{
-					var rx = (w/2 + bw) + 'px';
-					var ry = (h/2 + bw) + 'px';
-					var val = rx + ' ' + ry;
-					domNode.style.borderTopLeftRadius = val;
-					domNode.style.borderTopRightRadius = val;
-					domNode.style.borderBottomLeftRadius = val;
-					domNode.style.borderBottomRightRadius = val;
-				}
+			var bw, rx, ry;
+			var bwString = domStyle.get(domNode, 'borderWidth');
+			if(bwString){
+				bw = parseFloat(bwString);
+			}
+			var w = domNode.offsetWidth;
+			var h = domNode.offsetHeight;
+			if(isNaN(bw) || bw < 0 || w <= 0 || h <= 0){
+				domNode.style.borderTopLeftRadius = '';
+				domNode.style.borderTopRightRadius = '';
+				domNode.style.borderBottomLeftRadius = '';
+				domNode.style.borderBottomRightRadius = '';
+			}else{
+				var rx = (w/2 + bw) + 'px';
+				var ry = (h/2 + bw) + 'px';
+				var val = rx + ' ' + ry;
+				domNode.style.borderTopLeftRadius = val;
+				domNode.style.borderTopRightRadius = val;
+				domNode.style.borderBottomLeftRadius = val;
+				domNode.style.borderBottomRightRadius = val;
 			}
 		}
 		

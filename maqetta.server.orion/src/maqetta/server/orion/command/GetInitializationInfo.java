4e31f12edb120b3eafbe14de5df9f7b5a562f44d
Bill Reed <billreed@us.ibm.com>
2013-01-29 13:10:45 -0500

    #3705 formatting changes

--- maqetta.server.orion/src/maqetta/server/orion/command/GetInitializationInfo.java
+++ maqetta.server.orion/src/maqetta/server/orion/command/GetInitializationInfo.java
@@ -23,100 +23,116 @@ import org.maqetta.server.ServerManager;
 
 public class GetInitializationInfo extends Command {
 	private String siteConigJson = null;
-	
+
 	private class MaqettaConfigException extends Exception {
 		private static final long serialVersionUID = 1L;
 
 		public MaqettaConfigException(String message) {
 			super(message);
 		}
-		
+
 	};
 
-    @Override
-    public void handleCommand(HttpServletRequest req, HttpServletResponse resp, IUser user) throws IOException {
-    	IEclipsePreferences users = new OrionScope().getNode("Users"); //$NON-NLS-1$
-		IEclipsePreferences result = (IEclipsePreferences) users.node(user.getUserID());
-    	String workbenchSettings = result.get(MaqettaOrionServerConstants.WORKBENCH_PREF, "{}");
-    	try {
-	    	String c = this.getSiteJson();
-			this.responseString=
-				"{\n"+
-				"\t'workbenchState':"+workbenchSettings+",\n"+
-				"\t'userInfo':{'userId': '"+user.getUserID()+"',"+
-				"\t\t'isLocalInstall': '"+String.valueOf(ServerManager.LOCAL_INSTALL)+"',"+
-				"\t\t'userFirstName': '"+String.valueOf(user.getPerson().getFirstName())+"',"+
-				"\t\t'userLastName': '"+String.valueOf(user.getPerson().getLastName())+"',"+
-				"\t\t'email': '"+user.getPerson().getEmail()+"'\n"+
-				"\t}"+
-				"\t"+c+"\n"+
-				"}";
-    	} catch (MaqettaConfigException e) {
-			System.err.println("Maqetta Configuration Exception: " + e.getMessage());
+	@Override
+	public void handleCommand(HttpServletRequest req, HttpServletResponse resp,
+			IUser user) throws IOException {
+		IEclipsePreferences users = new OrionScope().getNode("Users"); //$NON-NLS-1$
+		IEclipsePreferences result = (IEclipsePreferences) users.node(user
+				.getUserID());
+		String workbenchSettings = result.get(
+				MaqettaOrionServerConstants.WORKBENCH_PREF, "{}");
+		try {
+			String c = this.getSiteJson();
+			this.responseString = "{\n" + "\t'workbenchState':"
+					+ workbenchSettings + ",\n" + "\t'userInfo':{'userId': '"
+					+ user.getUserID() + "'," + "\t\t'isLocalInstall': '"
+					+ String.valueOf(ServerManager.LOCAL_INSTALL) + "',"
+					+ "\t\t'userFirstName': '"
+					+ String.valueOf(user.getPerson().getFirstName()) + "',"
+					+ "\t\t'userLastName': '"
+					+ String.valueOf(user.getPerson().getLastName()) + "',"
+					+ "\t\t'email': '" + user.getPerson().getEmail() + "'\n"
+					+ "\t}" + "\t" + c + "\n" + "}";
+		} catch (MaqettaConfigException e) {
+			System.err.println("Maqetta Configuration Exception: "
+					+ e.getMessage());
 			e.printStackTrace();
-			//TODO: throw a 500, for now.  Consider whether we should send this error back in JSON instead
+			// TODO: throw a 500, for now. Consider whether we should send this
+			// error back in JSON instead
 			throw new IOException(e.getMessage());
 		}
 
-    }
-    
-    private String getSiteJson() throws MaqettaConfigException{
-    	
-    	if (this.siteConigJson != null) {
-    		return this.siteConigJson;
-    	}
-    	String ret = "";
-    	String siteConfigDir =  ServerManager.getServerManger().getDavinciProperty(IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY);
-    	if (siteConfigDir == null) {
-    		throw new MaqettaConfigException("maqetta.server.orion.command.GetInitializationInfo " + IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY + " :  Is not set in Config file");
-    		
-    	}
-			 File folder = new File(siteConfigDir);
-			 if (folder.exists()) {
-				 File[] listOfFiles = folder.listFiles(); 
-		    	 
-		    	  for (int i = 0; i < listOfFiles.length; i++) 
-		    	  {
-		    	 
-		    	   if (listOfFiles[i].isFile()) 
-		    	   {
-		    		   String file = listOfFiles[i].getName();
-		    	       if (file.endsWith(".json") || file.endsWith(".JSON"))
-		    	       {
-		    	    	   try {
-		    	    		   String fileNameWithOutExt = file.replaceFirst("[.][^.]+$", "");
-		    	    		   String output = this.readFile(siteConfigDir+"/"+file);
-		    	    		   JSONObject j = new JSONObject(output);
-		    	    		   ret = ret + ",\n\t'"+fileNameWithOutExt+"': "+output;
-		    	    		}
-		    	    		catch(JSONException ex) {
-		    	        		throw new MaqettaConfigException("maqetta.server.orion.command.GetInitializationInfo "+siteConfigDir+"/"+file + " not valid json");
-		    	    		} 
-		    	    	   catch (IOException e) {
-		    	       		   throw new MaqettaConfigException("maqetta.server.orion.command.GetInitializationInfo "+siteConfigDir+"/"+file + " error reading file");
-							}
-		    	        }
-		    	     }
-		    	  }
-	
-	    	 } else {
-	     		throw new MaqettaConfigException("maqetta.server.orion.command.GetInitializationInfo " + IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY + " : " + siteConfigDir + " Does not exist");
-	    	 }
-    	  this.siteConigJson = ret;
-    	  return this.siteConigJson;
-    }
-    
-    private String readFile(String path) throws IOException {
-    	  FileInputStream stream = new FileInputStream(new File(path));
-    	  try {
-    	    FileChannel fc = stream.getChannel();
-    	    MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-    	    /* Instead of using default, pass in a decoder. */
-    	    return Charset.defaultCharset().decode(bb).toString();
-    	  }
-    	  finally {
-    	    stream.close();
-    	  }
-    	}
+	}
+
+	private String getSiteJson() throws MaqettaConfigException {
+
+		if (this.siteConigJson != null) {
+			return this.siteConigJson;
+		}
+		String ret = "";
+		String siteConfigDir = ServerManager.getServerManger()
+				.getDavinciProperty(
+						IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY);
+		if (siteConfigDir == null) {
+			throw new MaqettaConfigException(
+					"maqetta.server.orion.command.GetInitializationInfo "
+							+ IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY
+							+ " :  Is not set in Config file");
+
+		}
+		File folder = new File(siteConfigDir);
+		if (folder.exists()) {
+			File[] listOfFiles = folder.listFiles();
+
+			for (int i = 0; i < listOfFiles.length; i++) {
+
+				if (listOfFiles[i].isFile()) {
+					String file = listOfFiles[i].getName();
+					if (file.endsWith(".json") || file.endsWith(".JSON")) {
+						try {
+							String fileNameWithOutExt = file.replaceFirst(
+									"[.][^.]+$", "");
+							String output = this.readFile(siteConfigDir + "/"
+									+ file);
+							JSONObject j = new JSONObject(output);
+							ret = ret + ",\n\t'" + fileNameWithOutExt + "': "
+									+ output;
+						} catch (JSONException ex) {
+							throw new MaqettaConfigException(
+									"maqetta.server.orion.command.GetInitializationInfo "
+											+ siteConfigDir + "/" + file
+											+ " not valid json");
+						} catch (IOException e) {
+							throw new MaqettaConfigException(
+									"maqetta.server.orion.command.GetInitializationInfo "
+											+ siteConfigDir + "/" + file
+											+ " error reading file");
+						}
+					}
+				}
+			}
+
+		} else {
+			throw new MaqettaConfigException(
+					"maqetta.server.orion.command.GetInitializationInfo "
+							+ IDavinciServerConstants.SITECONFIG_DIRECTORY_PROPERTY
+							+ " : " + siteConfigDir + " Does not exist");
+		}
+		this.siteConigJson = ret;
+		return this.siteConigJson;
+	}
+
+	private String readFile(String path) throws IOException {
+		FileInputStream stream = new FileInputStream(new File(path));
+		try {
+			FileChannel fc = stream.getChannel();
+			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0,
+					fc.size());
+			/* Instead of using default, pass in a decoder. */
+			return Charset.defaultCharset().decode(bb).toString();
+		} finally {
+			stream.close();
+		}
+	}
 
 }

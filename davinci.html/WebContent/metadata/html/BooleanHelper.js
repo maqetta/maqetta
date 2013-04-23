695c7afd7402bfd5c41167e56d701adf22383c65
Bill Reed <billreed@us.ibm.com>
2013-04-23 18:19:04 -0400

    #3938 refactor

--- davinci.html/WebContent/metadata/html/BooleanHelper.js
+++ davinci.html/WebContent/metadata/html/BooleanHelper.js
@@ -12,69 +12,20 @@ define(function() {
 			 * we need to remove the attribute to make HTML happy
 			 * these are all the boolean attributes.
 			 */
+			var boolAttr = ['disabled', 'checked', 'autofocus', 'readonly', 
+			                'required', 'multiple', 'autoplay', 'controls', 
+			                'loop', 'muted'
+			                ];
+	
 			if (widget.properties) {
-				widget.domNode.disabled = widget.properties.disabled;
-				if (widget.domNode.disabled == false) {
-					widget.domNode.removeAttribute('disabled');
-				} else {
-					widget.domNode.setAttribute('disabled', true);
-				}
-				widget.domNode.checked = widget.properties.checked;
-				if (widget.domNode.checked == false) {
-					widget.domNode.removeAttribute('checked');
-				} else {
-					widget.domNode.setAttribute('checked', true);
-				}
-				widget.domNode.autofocus = widget.properties.autofocus;
-				if (widget.domNode.autofocus == false) {
-					widget.domNode.removeAttribute('autofocus');
-				} else {
-					widget.domNode.setAttribute('autofocus', true);
-				}
-				widget.domNode.readonly = widget.properties.readonly;
-				if (widget.domNode.readonly == false) {
-					widget.domNode.removeAttribute('readonly');
-				} else {
-					widget.domNode.setAttribute('readonly', true);
-				}
-				widget.domNode.required = widget.properties.required;
-				if (widget.domNode.required == false) {
-					widget.domNode.removeAttribute('required');
-				} else {
-					widget.domNode.setAttribute('required', true);
-				}
-				widget.domNode.multiple = widget.properties.multiple;
-				if (widget.domNode.multiple == false) {
-					widget.domNode.removeAttribute('multiple');
-				} else {
-					widget.domNode.setAttribute('multiple', true);
-				}
-				widget.domNode.autoplay = widget.properties.autoplay;
-				if (widget.domNode.autoplay == false) {
-					widget.domNode.removeAttribute('autoplay');
-				} else {
-					widget.domNode.setAttribute('autoplay', true);
-				}
-				widget.domNode.controls = widget.properties.controls;
-				if (widget.domNode.controls == false) {
-					widget.domNode.removeAttribute('controls');
-				} else {
-					widget.domNode.setAttribute('controls', true);
-				}
-				widget.domNode.loop = widget.properties.loop;
-				if (widget.domNode.loop == false) {
-					widget.domNode.removeAttribute('loop');
-				} else {
-					widget.domNode.setAttribute('loop', true);
-				}
-				widget.domNode.muted = widget.properties.muted;
-				if (widget.domNode.muted == false) {
-					widget.domNode.removeAttribute('muted');
-				} else {
-					widget.domNode.setAttribute('muted', true);
-				}
-				
-				
+				boolAttr.forEach(function(attr){
+					widget.domNode[attr] = widget.properties[attr];
+					if (widget.domNode[attr] == false) {
+						widget.domNode.removeAttribute(attr);
+					} else {
+						widget.domNode.setAttribute(attr, true);
+					}
+				});
 			}
 		}
 

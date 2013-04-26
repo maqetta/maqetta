9b9f43a3749708efc3c5f42558c48d60b8f0e777
Bill Reed <billreed@us.ibm.com>
2013-04-26 10:13:50 -0400

    #3475 put check for IE in dojo.ready

--- maqetta.server.orion.authentication.ldap/web/js/LoginWindow.js
+++ maqetta.server.orion.authentication.ldap/web/js/LoginWindow.js
@@ -14,29 +14,34 @@
 
 define([
 	'dojo/request/xhr',
-	'orion/PageUtil'
+	'orion/PageUtil',
+	"dojo/ready"
 ], function(
 	xhr,
-	PageUtil
+	PageUtil,
+	ready
 ) {
-	var ua = window.navigator.userAgent;
-	var ieIndex = ua.indexOf('MSIE');
-	var isIE = (ieIndex>=0) ? parseInt(ua.substr(ieIndex+4), 10) : false;
-	if(isIE){
-		var browser_not_supported = document.getElementById("browser_not_supported");
-		browser_not_supported.style.display = "";
-		browser_not_supported.style.color = "red";
-		browser_not_supported.style.fontSize = "16px";
-		browser_not_supported.style.padding = "20px 15px";
-		return;
-	}
-	var userCreationEnabled;
-	var registrationURI;
-
-	var GENERIC_SERVER_MSG = 'A unexpected error prevented the operation from completing.  ' +
-			'Please try again.  If the problem persists, please contact the server administrator.';
-	var ADMIN_USERID = 'admin';
-	var ID_PREFIX = '00MaqTempId00'; // keep in sync w/ LoginFixUpFilter.java
+	
+	ready(function(){
+		var ua = window.navigator.userAgent;
+		var ieIndex = ua.indexOf('MSIE');
+		var isIE = (ieIndex>=0) ? parseInt(ua.substr(ieIndex+4), 10) : false;
+		if(isIE){
+			var browser_not_supported = document.getElementById("browser_not_supported");
+			browser_not_supported.style.display = "";
+			browser_not_supported.style.color = "red";
+			browser_not_supported.style.fontSize = "16px";
+			browser_not_supported.style.padding = "20px 15px";
+			return;
+		}
+		var userCreationEnabled;
+		var registrationURI;
+	
+		var GENERIC_SERVER_MSG = 'A unexpected error prevented the operation from completing.  ' +
+				'Please try again.  If the problem persists, please contact the server administrator.';
+		var ADMIN_USERID = 'admin';
+		var ID_PREFIX = '00MaqTempId00'; // keep in sync w/ LoginFixUpFilter.java
+	 });
 
 	function injectPlaceholderShims() {
 		function textFocus(e) {
--- maqetta.server.orion/WebContent/login/LoginWindow.js
+++ maqetta.server.orion/WebContent/login/LoginWindow.js
@@ -14,22 +14,28 @@
 
 define([
 	'dojo/request/xhr',
-	'orion/PageUtil'
+	'orion/PageUtil',
+	"dojo/ready"
 ], function(
 	xhr,
-	PageUtil
+	PageUtil,
+	ready
 ) {
-	var ua = window.navigator.userAgent;
-	var ieIndex = ua.indexOf('MSIE');
-	var isIE = (ieIndex>=0) ? parseInt(ua.substr(ieIndex+4), 10) : false;
-	if(isIE){
-		var browser_not_supported = document.getElementById("browser_not_supported");
-		browser_not_supported.style.display = "";
-		browser_not_supported.style.color = "red";
-		browser_not_supported.style.fontSize = "16px";
-		browser_not_supported.style.padding = "20px 15px";
-		return;
-	}
+
+	 ready(function(){
+			var ua = window.navigator.userAgent;
+			var ieIndex = ua.indexOf('MSIE');
+			var isIE = (ieIndex>=0) ? parseInt(ua.substr(ieIndex+4), 10) : false;
+			if(isIE){
+				var browser_not_supported = document.getElementById("browser_not_supported");
+				browser_not_supported.style.display = "";
+				browser_not_supported.style.color = "red";
+				browser_not_supported.style.fontSize = "16px";
+				browser_not_supported.style.padding = "20px 15px";
+				return;
+			}
+			
+	  });
 	var userCreationEnabled;
 	var registrationURI;
 
